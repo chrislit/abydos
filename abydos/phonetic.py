29a7ef2720cdbacc33dafc44f949ef08b733fa0b
Christopher C. Little <chrisclittle@gmail.com>
2014-05-15 06:21:33 -0700

    converted soundex-style algorithms to operate on uppercase (as in the original algorithms)

--- abydos/phonetic.py
+++ abydos/phonetic.py
@@ -10,9 +10,9 @@ def russell_index(word):
     """Given a string 'word', returns its Int value according to Robert C.
     Russell's Index algorithm, as described in US Patent 1,261,167 (1917)
     """
-    word = word.lower()
-    word = word.replace('gh', '')  # discard gh (rule 3)
-    while word[len(word)-1] in 'sz':
+    word = word.upper()
+    word = word.replace('GH', '')  # discard gh (rule 3)
+    while word[len(word)-1] in 'SZ':
         word = word[:len(word)-2] # discard /[sz]$/ (rule 3)
 
     """
@@ -22,21 +22,21 @@ def russell_index(word):
 
     sdx = ''
     for char in word:
-        if char in 'aeiouy' and '1' not in sdx: # rule 1: "oral resonants"
+        if char in 'AEIOUY' and '1' not in sdx: # rule 1: "oral resonants"
             sdx += '1'
-        elif char in 'bfpv': # rule 2: "labials and labio-dentals"
+        elif char in 'BFPV': # rule 2: "labials and labio-dentals"
             sdx += '2'
-        elif char in 'cgkqxsz': # rule 3: "gutturals and ...  sibilants"
+        elif char in 'CGKQXSZ': # rule 3: "gutturals and ...  sibilants"
             sdx += '3'
-        elif char in 'dt': # rule 4: "dental-mutes"
+        elif char in 'DT': # rule 4: "dental-mutes"
             sdx += '4'
-        elif char is 'l': # rule 5: "palatal fricative"
+        elif char is 'L': # rule 5: "palatal fricative"
             sdx += '5'
-        elif char is 'm': # rule 6: "labio-nasal"
+        elif char is 'M': # rule 6: "labio-nasal"
             sdx += '6'
-        elif char is 'n': # rule 7: "dento or lingua-nasal"
+        elif char is 'N': # rule 7: "dento or lingua-nasal"
             sdx += '7'
-        elif char is 'r': # rule 8: "dental-fricative"
+        elif char is 'R': # rule 8: "dental-fricative"
             sdx += '8'
 
     sdx = _delete_consecutive_repeats(sdx)
@@ -48,7 +48,7 @@ def russell_index_num_to_alpha(num):
     alphabetic form, as described in US Patent 1,261,167 (1917)
     """
     num = str(num)
-    return num.translate(maketrans('12345678', 'abcdlmnr'), '09')
+    return num.translate(maketrans('12345678', 'ABCDLMNR'), '09')
 
 
 def russell_index_alpha(word):
@@ -62,24 +62,24 @@ def knuth_soundex(word):
     """As descibed in Knuth(1998:394)
     """
     sdx = ''
-    word = word.lower()
+    word = word.upper()
     for char in word:
-        if char in 'bfpv': # rule 2a
+        if char in 'BFPV': # rule 2a
             sdx += '1'
-        elif char in 'cgjkqsxz': # rule 2b
+        elif char in 'CGJKQSXZ': # rule 2b
             sdx += '2'
-        elif char in 'dt': # rule 2c
+        elif char in 'DT': # rule 2c
             sdx += '3'
-        elif char is 'l': # rule 2d
+        elif char is 'L': # rule 2d
             sdx += '4'
-        elif char in 'mn': # rule 2e
+        elif char in 'MN': # rule 2e
             sdx += '5'
-        elif char is 'r': # rule 2f
+        elif char is 'R': # rule 2f
             sdx += '6'
 
-        elif char in 'aeiouy': # rule 1
+        elif char in 'AEIOUY': # rule 1
             sdx += '0'
-        elif char in 'hw': # rule 1
+        elif char in 'HW': # rule 1
             sdx += '9'
 
         else:
@@ -99,18 +99,18 @@ def knuth_soundex(word):
 def creativyst_soundex(word):
     """Incorporates enhancements from http://creativyst.com/Doc/Articles/SoundEx1/SoundEx1.htm#Enhancements
     """
-    word = word.lower()
-    word = word.replace('dg', 'g')
-    word = re.sub(r'(.)gn', r'\1n', word)
-    word = word.replace('gn', 'n')
-    word = word.replace('kn', 'n')
-    word = word.replace('ph', 'f')
-    word = re.sub('mp([szt])', 'm\1', word)
-    word = word.replace('^ps', 's')
-    word = word.replace('^pf', 'f')
-    word = word.replace('mb', 'm')
-    word = word.replace('tch', 'ch')
-    word = re.sub('^[ai]([aeio])', 'e', word)
+    word = word.upper()
+    word = word.replace('DG', 'G')
+    word = re.sub(r'(.)GN', r'\1N', word)
+    word = word.replace('GN', 'N')
+    word = word.replace('KN', 'N')
+    word = word.replace('PH', 'F')
+    word = re.sub('MP([SZT])', 'M\1', word)
+    word = word.replace('^PS', 'S')
+    word = word.replace('^PF', 'F')
+    word = word.replace('MB', 'M')
+    word = word.replace('TCH', 'CH')
+    word = re.sub('^[AI]([AEIO])', 'E', word)
 
     return knuth_soundex(word)
 
@@ -129,57 +129,59 @@ def koelner_phonetik(word):
             return True
         return False
 
-    word = word.lower()
     sdx = ''
 
-    word = word.replace('ä', 'ae')
-    word = word.replace('ö', 'oe')
-    word = word.replace('ü', 'ue')
-    word = word.replace('ß', 'ss')
+    word = word.replace('ß', 'SS')
+    word = word.upper()
+
+    word = word.replace('Ä', 'AE')
+    word = word.replace('Ö', 'OE')
+    word = word.replace('Ü', 'UE')
+
 
     for i in range(len(word)):
-        if word[i] in 'aeijyou':
+        if word[i] in 'AEIJYOU':
             sdx += '0'
-        elif word[i] in 'b':
+        elif word[i] in 'B':
             sdx += '1'
-        elif word[i] in 'p':
-            if _before(word, i, 'h'):
+        elif word[i] in 'P':
+            if _before(word, i, 'H'):
                 sdx += '3'
             else:
                 sdx += '1'
-        elif word[i] in 'dt':
-            if _before(word, i, 'csz'):
+        elif word[i] in 'DT':
+            if _before(word, i, 'CSZ'):
                 sdx += '8'
             else:
                 sdx += '2'
-        elif word[i] in 'fvw':
+        elif word[i] in 'FVW':
             sdx += '3'
-        elif word[i] in 'gkq':
+        elif word[i] in 'GKQ':
             sdx += '4'
-        elif word[i] in 'c':
-            if _after(word, i, 'sz'):
+        elif word[i] in 'C':
+            if _after(word, i, 'SZ'):
                 sdx += '8'
             elif i == 0:
-                if _after(word, i, 'ahkloqrux'):
+                if _after(word, i, 'AHKLOQRUX'):
                     sdx += '4'
                 else:
                     sdx += '8'
-            elif _before(word, i, 'ahkoqux'):
+            elif _before(word, i, 'AHKOQUX'):
                 sdx += '4'
             else:
                 sdx += '8'
-        elif word[i] in 'x':
-            if _after(word, i, 'ckq'):
+        elif word[i] in 'X':
+            if _after(word, i, 'CKQ'):
                 sdx += '8'
             else:
                 sdx += '48'
-        elif word[i] in 'l':
+        elif word[i] in 'L':
             sdx += '5'
-        elif word[i] in 'mn':
+        elif word[i] in 'MN':
             sdx += '6'
-        elif word[i] in 'r':
+        elif word[i] in 'R':
             sdx += '7'
-        elif word[i] in 'sz':
+        elif word[i] in 'SZ':
             sdx += '8'
 
     sdx = _delete_consecutive_repeats(sdx)
@@ -194,7 +196,7 @@ def koelner_phonetik_num_to_alpha(num):
     alphabetic form
     """
     num = str(num)
-    return num.translate(maketrans('012345678', 'aptfklnrs'))
+    return num.translate(maketrans('012345678', 'APTFKLNRS'))
 
 
 def koelner_phonetik_alpha(word):
