b5938e7cf9f243c16ca3a6793fdbcfa0ef94ad77
Christopher C. Little <chrisclittle@gmail.com>
2014-05-16 23:56:19 -0700

    fingerprint function signatures

--- abydos/clustering.py
+++ abydos/clustering.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 
+def fingerprint(phrase):
+    pass
+
+def qgram_fingerprint(phrase, q=2):
+    pass
+
+def phonetic_fingerprint(phrase, phonetic_algorithm, args):
+    pass
