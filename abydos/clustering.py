9fc6e3aa434fe63b8b220921b04a052331c6c926
Christopher C. Little <chrisclittle@gmail.com>
2014-05-18 02:21:05 -0700

    added default args for phonetic_fingerprint()

--- abydos/clustering.py
+++ abydos/clustering.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import abydos.phonetic
+
 def fingerprint(phrase):
     pass
 
 def qgram_fingerprint(phrase, q=2):
     pass
 
-def phonetic_fingerprint(phrase, phonetic_algorithm, args):
+def phonetic_fingerprint(phrase, phonetic_algorithm=abydos.phonetic.double_metaphone, args=None):
     pass
