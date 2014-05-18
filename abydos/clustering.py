5d43768158be65c5c9453a63376c740b9315dd2f
Christopher C. Little <chrisclittle@gmail.com>
2014-05-18 03:45:46 -0700

    added qgram & phonetic fingerprint functions fixed tests

--- abydos/clustering.py
+++ abydos/clustering.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import abydos.phonetic
+from .phonetic import double_metaphone
 import unicodedata
+from .util import qgrams
 
 def fingerprint(phrase):
     phrase = unicodedata.normalize('NFKD', unicode(phrase.strip().lower()))
     phrase = filter(lambda c: (c.isalnum() or c.isspace()), phrase)
-    phrase = " ".join(sorted(list(set(phrase.split()))))
+    phrase = ' '.join(sorted(list(set(phrase.split()))))
     return phrase
 
 def qgram_fingerprint(phrase, q=2):
-    pass
+    phrase = unicodedata.normalize('NFKD', unicode(phrase.strip().lower()))
+    phrase = filter(lambda c: c.isalnum(), phrase)
+    phrase = qgrams(phrase, q)
+    phrase = ''.join(sorted(list(set(phrase))))
+    return phrase
 
-def phonetic_fingerprint(phrase, phonetic_algorithm=abydos.phonetic.double_metaphone, args=None):
-    pass
+def phonetic_fingerprint(phrase, phonetic_algorithm=double_metaphone, *args):
+    phrase = phonetic_algorithm(phrase, *args)
+    if not isinstance(phrase, unicode):
+        phrase = phrase[0]
+    return fingerprint(phrase)
--- abydos/tests/test_clustering.py
+++ abydos/tests/test_clustering.py
@@ -8,7 +8,7 @@ from abydos.clustering import fingerprint, qgram_fingerprint, \
 testset = ('À noite, vovô Kowalsky vê o ímã cair no pé do pingüim queixoso e vovó põe açúcar no chá de tâmaras do jabuti feliz.',)
 anssetw = ('a acucar cair cha de do e feliz ima jabuti kowalsky no noite o pe pinguim poe queixoso tamaras ve vovo',)
 anssetq2 = ('abacadaialamanarasbucachcudedoeaedeieleoetevfeguhaifiminiritixizjakokylilsmamqngnoocoeoiojokoposovowpepipoqurarnsdsksotatetiucueuiutvevowaxoyv',)
-anssetq1 = ('abcdefghijklmnopqrstuvwxyz')
+anssetq1 = ('abcdefghijklmnopqrstuvwxyz',)
 
 class fingerprint_test_cases(unittest.TestCase):
     def test_fingerprint(self):
