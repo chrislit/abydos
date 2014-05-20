40c97b6e227f213d19ed01e649b384e4955b00b6
Christopher C. Little <chrisclittle@gmail.com>
2014-05-20 00:06:04 -0700

    stats test stubs

--- abydos/tests/test_stats.py
+++ abydos/tests/test_stats.py
@@ -2,3 +2,121 @@
 
 from __future__ import unicode_literals
 import unittest
+from abydos.stats import ConfusionTable
+
+class cast_test_cases(unittest.TestCase):
+    def test_tuple(self):
+        pass
+
+    def test_dict(self):
+        pass
+
+
+class population_test_cases(unittest.TestCase):
+    def test_correct_pop(self):
+        pass
+
+    def test_error_pop(self):
+        pass
+
+    def test_test_pos_pop(self):
+        pass
+
+    def test_test_neg_pop(self):
+        pass
+
+    def test_cond_pos_pop(self):
+        pass
+
+    def test_cond_neg_pop(self):
+        pass
+
+    def test_population(self):
+        pass
+
+
+class statistical_ratio_test_cases(unittest.TestCase):
+    def test_precision(self):
+        pass
+
+    def test_recall(self):
+        pass
+
+    def test_specificity(self):
+        pass
+
+    def test_npv(self):
+        pass
+
+    def test_fallout(self):
+        pass
+
+    def test_fdr(self):
+        pass
+
+    def test_accuracy(self):
+        pass
+
+    def test_balanced_accuracy(self):
+        pass
+
+    def test_informedness(self):
+        pass
+
+    def test_markedness(self):
+        pass
+
+
+class pr_means_test_cases(unittest.TestCase):
+    def test_pr_mean(self):
+        pass
+
+    def test_pr_gmean(self):
+        pass
+
+    def test_pr_hmean(self):
+        pass
+
+    def test_pr_qmean(self):
+        pass
+
+    def test_pr_lmean(self):
+        pass
+
+    def test_pr_cmean(self):
+        pass
+
+    def test_pr_imean(self):
+        pass
+
+    def test_pr_pmean(self):
+        pass
+
+
+class statistical_measure_test_cases(unittest.TestCase):
+    def test_fbeta_score(self):
+        pass
+
+    def test_f2_score(self):
+        pass
+
+    def test_fhalf_score(self):
+        pass
+
+    def test_e_score(self):
+        pass
+
+    def test_f1_score(self):
+        pass
+
+    def test_f_measure(self):
+        pass
+
+    def test_g_measure(self):
+        pass
+
+    def test_mcc(self):
+        pass
+
+    def test_significance(self):
+        pass
