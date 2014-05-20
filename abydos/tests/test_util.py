30838a43469031db17118398223e6f93cda07d7c
Christopher C. Little <chrisclittle@gmail.com>
2014-05-20 00:08:27 -0700

    util test stubs

--- abydos/tests/test_util.py
+++ abydos/tests/test_util.py
@@ -2,3 +2,14 @@
 
 from __future__ import unicode_literals
 import unittest
+from abydos.util import qgrams, _qgram_lists, _qgram_counts
+
+class qgram_test_cases(unittest.TestCase):
+    def test_qgrams(self):
+        pass
+
+    def test_qgram_lists(self):
+        pass
+
+    def test_qgram_counts(self):
+        pass
