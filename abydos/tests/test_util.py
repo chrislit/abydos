51e786ac4dac47b5d1737615b952b33036222670
Christopher C. Little <chrisclittle@gmail.com>
2014-05-20 03:32:50 -0700

    added tests for q-gram util functions

--- abydos/tests/test_util.py
+++ abydos/tests/test_util.py
@@ -6,10 +6,32 @@ from abydos.util import qgrams, _qgram_lists, _qgram_counts
 
 class qgram_test_cases(unittest.TestCase):
     def test_qgrams(self):
-        pass
+        self.assertEquals(qgrams(''), [])
+        self.assertEquals(qgrams('NELSON', 3), ['NEL', 'ELS', 'LSO', 'SON'])
+        self.assertEquals(qgrams('NELSON', 6), ['NELSON'])
+        self.assertEquals(qgrams('NELSON', 7), [])
+
+        #http://www.sound-ex.com/alternative_qgram.htm
+        self.assertEquals(qgrams('NELSON'), ['NE', 'EL', 'LS', 'SO', 'ON'])
+        self.assertEquals(qgrams('NEILSEN'),
+                          ['NE', 'EI', 'IL', 'LS', 'SE', 'EN'])
 
     def test_qgram_lists(self):
-        pass
+        self.assertEquals(_qgram_lists('NELSON', ''),
+                          (['NE', 'EL', 'LS', 'SO', 'ON'], [], []))
+        self.assertEquals(_qgram_lists('', 'NEILSEN'),
+                          ([], ['NE', 'EI', 'IL', 'LS', 'SE', 'EN'], []))
+        self.assertEquals(_qgram_lists('NELSON', 'NEILSEN'),
+                          (['NE', 'EL', 'LS', 'SO', 'ON'],
+                           ['NE', 'EI', 'IL', 'LS', 'SE', 'EN'],
+                           ['NE', 'LS']))
+        self.assertEquals(_qgram_lists('NELSON', 'NOSLEN'),
+                          (['NE', 'EL', 'LS', 'SO', 'ON'],
+                           ['NO', 'OS', 'SL', 'LE', 'EN'],
+                           []))
 
     def test_qgram_counts(self):
-        pass
+        self.assertEquals(_qgram_counts('NELSON', ''), (5, 0, 0))
+        self.assertEquals(_qgram_counts('', 'NEILSEN'), (0, 6, 0))
+        self.assertEquals(_qgram_counts('NELSON', 'NEILSEN'), (5, 6, 2))
+        self.assertEquals(_qgram_counts('NELSON', 'NOSLEN'), (5, 5, 0))
