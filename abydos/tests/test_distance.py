1230e8f0253fc655a8b23a60d5e3e9a12d160607
Christopher C. Little <chrisclittle@gmail.com>
2014-05-19 23:26:13 -0700

    added distance test stubs

--- abydos/tests/test_distance.py
+++ abydos/tests/test_distance.py
@@ -2,3 +2,63 @@
 
 from __future__ import unicode_literals
 import unittest
+from abydos.distance import levenshtein, hamming, tversky_index, \
+    sorensen_coeff, sorensen, jaccard_coeff, jaccard, tanimoto_coeff, \
+    tanimoto, strcmp95, jaro_winkler, lcs, mra_compare
+
+
+class levenshtein_test_cases(unittest.TestCase):
+    def test_levenshtein(self):
+        pass
+
+
+class hamming_test_cases(unittest.TestCase):
+    def test_hamming(self):
+        pass
+
+
+class tversky_index_test_cases(unittest.TestCase):
+    def test_tversky_index(self):
+        pass
+
+
+class sorensen_test_cases(unittest.TestCase):
+    def test_sorensen_coeff(self):
+        pass
+
+    def test_sorensen(self):
+        pass
+
+
+class jaccard_test_cases(unittest.TestCase):
+    def test_jaccard_coeff(self):
+        pass
+
+    def test_jaccard(self):
+        pass
+
+
+class tanimoto_test_cases(unittest.TestCase):
+    def test_tanimoto_coeff(self):
+        pass
+
+    def test_tanimoto(self):
+        pass
+
+
+class jaro_winkler_test_cases(unittest.TestCase):
+    def test_strcmp95(self):
+        pass
+
+    def test_jaro_winkler(self):
+        pass
+
+
+class lcs_test_cases(unittest.TestCase):
+    def test_lcs(self):
+        pass
+
+
+class mra_compare_test_cases(unittest.TestCase):
+    def test_mra_compare(self):
+        pass
