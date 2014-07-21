c323192f42a540bd888d20130ad8d9c394e8eb5a
Chris Little <chrisclittle@gmail.com>
2014-07-20 20:06:24 -0700

    added copyright/license notice to BM data

--- abydos/bm/gen_rules_romanian.py
+++ abydos/bm/gen_rules_romanian.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
+"""abydos.distance
+
+Copyright 2014 by Christopher C. Little.
+This file is part of Abydos.
+
+This file is derived from PHP code by Alexander Beider and Stephen P. Morse that
+is part of the Beider-Morse Phonetic Matching (BMPM) System, available at
+http://stevemorse.org/phonetics/bmpm.htm.
+
+Abydos is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Abydos is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Abydos. If not, see <http://www.gnu.org/licenses/>.
+"""
              ('ce','','','tSe'),
              ('ci','','','(tSi|tS)'),
              ('ch','','[ei]','k'),
--- helpers/bm_php2py.py
+++ helpers/bm_php2py.py
@@ -29,6 +29,8 @@ for s in subdirs:
                     outfile = codecs.open(outfilename, 'w', 'utf-8')
                     print outfilename
 
+                    outfile.write('# -*- coding: utf-8 -*-\n\"\"\"abydos.distance\n\nCopyright 2014 by Christopher C. Little.\nThis file is part of Abydos.\n\nThis file is derived from PHP code by Alexander Beider and Stephen P. Morse that\nis part of the Beider-Morse Phonetic Matching (BMPM) System, available at\nhttp://stevemorse.org/phonetics/bmpm.htm.\n\nAbydos is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nAbydos is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with Abydos. If not, see <http://www.gnu.org/licenses/>.\n\"\"\"\n')
+
                     ignore = True
                     for line in infile:
                         if not ignore:
