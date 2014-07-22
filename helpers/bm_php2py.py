7d93bf65552c358a86f8f643d1ede357c772bdc3
Chris Little <chrisclittle@gmail.com>
2014-07-22 06:54:05 -0700

    moved BM data into a single module (bmdata.py) with a dict in dict structure for the rule tuples

--- helpers/bm_php2py.py
+++ helpers/bm_php2py.py
@@ -30,9 +30,15 @@ def pythonize(line, indent=0, fn=''):
 
 bmdir = sys.argv[1]
 
+outfilename = '../abydos/bmdata.py'
+outfile = codecs.open(outfilename, 'w', 'utf-8')
+outfile.write('# -*- coding: utf-8 -*-\n\"\"\"abydos.distance\n\nCopyright 2014 by Christopher C. Little.\nThis file is part of Abydos.\n\nThis file is derived from PHP code by Alexander Beider and Stephen P. Morse that\nis part of the Beider-Morse Phonetic Matching (BMPM) System, available at\nhttp://stevemorse.org/phonetics/bmpm.htm.\n\nAbydos is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nAbydos is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with Abydos. If not, see <http://www.gnu.org/licenses/>.\n\"\"\"\n\n')
+outfile.write('bmdata = dict()\n\n')
+
 subdirs = ('gen', 'sep', 'ash')
 
 for s in subdirs:
+    outfile.write('bmdata[\''+s+'\'] = dict()\n\n')
     phps = [f for f in listdir(bmdir + s + '/') if (isfile(bmdir + s + '/' + f)
                                                     and f.endswith('.php'))]
     for infilename in phps:
@@ -42,12 +48,9 @@ for s in subdirs:
                                          'r', 'utf-8')
                     tuplename = pfx + '_' + infilename[len(pfx):-4]
                     indent = len(tuplename) + 9
-                    outfilename = '../abydos/bm/' + s + '_' + tuplename + '.py'
-                    outfile = codecs.open(outfilename, 'w', 'utf-8')
-                    print outfilename
+                    print infilename
 
-                    outfile.write('# -*- coding: utf-8 -*-\n\"\"\"abydos.distance\n\nCopyright 2014 by Christopher C. Little.\nThis file is part of Abydos.\n\nThis file is derived from PHP code by Alexander Beider and Stephen P. Morse that\nis part of the Beider-Morse Phonetic Matching (BMPM) System, available at\nhttp://stevemorse.org/phonetics/bmpm.htm.\n\nAbydos is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nAbydos is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with Abydos. If not, see <http://www.gnu.org/licenses/>.\n\"\"\"\n\n')
-                    outfile.write('_' + s + '_' + tuplename + ' = (\n')
+                    outfile.write('bmdata[\'' + s + '\'][\'' + tuplename + '\'] = (\n')
 
                     ignore = True
                     for line in infile:
@@ -60,4 +63,4 @@ for s in subdirs:
                         if re.search('\$.+ = array\(', line):
                             ignore = False
 
-                    outfile.write(indent*' ' + ')\n')
+                    outfile.write(indent*' ' + ')\n\n')
