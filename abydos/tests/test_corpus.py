67cbbab7a3ca718d8e64139924ed17da6b0bd9af
Chris Little <chrisclittle@gmail.com>
2014-08-07 02:34:13 -0700

    added base case tests for Corpus class

---
+++ abydos/tests/test_corpus.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""abydos.tests.test_corpus
+
+This module contains unit tests for abydos.corpus
+
+Copyright 2014 by Christopher C. Little.
+This file is part of Abydos.
+
+Abydos is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Abydos is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Abydos. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from __future__ import unicode_literals
+import unittest
+from abydos.corpus import Corpus
+
+class QgramTestCases(unittest.TestCase):
+    """test cases for abydos.qgram
+    """
+    def test_qgrams(self):
+        """test abydos.qgram.QGrams
+        """
+        # base cases
+        self.assertEqual(Corpus().corpus, [])
+        self.assertEqual(Corpus('').corpus, [])
+        self.assertEqual(Corpus(' ').corpus, [])
+        self.assertEqual(Corpus('\n').corpus, [])
+        self.assertEqual(Corpus(' \n').corpus, [])
+        self.assertEqual(Corpus(' \n ').corpus, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
