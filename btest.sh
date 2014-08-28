e1586bace2b17557ddb07a9d211c12a3d8508624
Chris Little <chrisclittle@gmail.com>
2014-08-28 04:44:18 -0700

    added build & test script

---
+++ btest.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo rm -rf ./build
+
+python setup.py build
+sudo python setup.py install
+
+python3 setup.py build
+sudo python3 setup.py install
+
+nosetests -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
+#nosetests3 -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
+
+pylint --rcfile=pylint.rc abydos > pylint.log
