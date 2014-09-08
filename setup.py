b1761c21eababc20da7f72b323e0afba38ff1298
Chris Little <chrisclittle@gmail.com>
2014-09-07 17:44:37 -0700

    spacing

--- setup.py
+++ setup.py
@@ -2,29 +2,29 @@
 
 from distutils.core import setup
 setup(
-    name = "abydos",
-    packages = ["abydos"],
-    version = "1.0.0",
-    description = "Abydos NLP library",
-    author = "Christopher C. Little",
-    author_email = "chrisclittle@gmail.com",
-    url = "https://github.com/chrislit/abydos",
-    download_url = "https://github.com/chrislit/abydos/archive/master.zip",
-    keywords = ["nlp", "ai"],
-    classifiers = [
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Text Processing :: Indexing",
-        "Topic :: Text Processing :: Linguistic",
-        ],
-    long_description = """\
+      name = "abydos",
+      packages = ["abydos"],
+      version = "1.0.0",
+      description = "Abydos NLP library",
+      author = "Christopher C. Little",
+      author_email = "chrisclittle@gmail.com",
+      url = "https://github.com/chrislit/abydos",
+      download_url = "https://github.com/chrislit/abydos/archive/master.zip",
+      keywords = ["nlp", "ai"],
+      classifiers = [
+                     "Programming Language :: Python",
+                     "Programming Language :: Python :: 3",
+                     "Development Status :: 4 - Beta",
+                     "Environment :: Other Environment",
+                     "Intended Audience :: Developers",
+                     "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+                     "Operating System :: OS Independent",
+                     "Topic :: Scientific/Engineering :: Artificial Intelligence",
+                     "Topic :: Software Development :: Libraries :: Python Modules",
+                     "Topic :: Text Processing :: Indexing",
+                     "Topic :: Text Processing :: Linguistic",
+                     ],
+      long_description = """\
 Abydos NLP library
 ------------------
 
@@ -32,4 +32,4 @@ Includes ...
 
 This version requires Python 2.7 or later.
 """
-)
+    )
