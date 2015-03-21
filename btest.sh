5310f93dbbbd7baf9e6975aa9d4b748d7040e3de
Chris Little <chrisclittle@gmail.com>
2015-03-20 17:28:11 -0700

    re-enable nosetest3

--- btest.sh
+++ btest.sh
@@ -9,6 +9,6 @@ python3 setup.py build
 sudo python3 setup.py install
 
 nosetests -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
-#nosetests3 -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
+nosetests3 -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
 
 pylint --rcfile=pylint.rc abydos > pylint.log
