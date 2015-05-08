81a9c703633f1139001511a6df06a73d71a5f18b
Christopher C. Little <chrisclittle@gmail.com>
2015-05-08 12:51:30 -0700

    added cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo rm -rf ./abydos.egg-info
+sudo rm -rf ./dist
+sudo rm -rf ./build
+sudo rm -rf ./*.log
+sudo rm -rf ./.coverage
+sudo rm -rf */*.pyc
+sudo rm -rf */__pycache__
+sudo rm -rf *~
+sudo rm -rf .*~
+sudo rm -rf */*~
+sudo rm -rf */.*~
