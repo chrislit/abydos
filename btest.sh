543978cd4e7e6c8fe7bc10c827bdecd294ef7f60
Christopher C. Little <chrisclittle@gmail.com>
2015-05-08 13:29:03 -0700

    fixed order of building/installing

--- btest.sh
+++ btest.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
-sudo rm -rf ./abydos.egg-info
-sudo rm -rf ./dist
+./cleanup.sh
 
-sudo rm -rf ./build
 python setup.py build
-sudo python setup.py install
-
-sudo rm -rf ./build
 python3 setup.py build
+
+sudo python3 setup.py sdist
+sudo python3 setup.py bdist_wheel
+
+sudo python setup.py install
 sudo python3 setup.py install
 
 nosetests -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
 nosetests3 -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
 
 pylint --rcfile=pylint.rc abydos > pylint.log
-
-sudo python3 setup.py sdist
-sudo python3 setup.py bdist_wheel
 
 ./badge_update.py
