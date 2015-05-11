9e0d5525a67ac4643f8ff2c97b4da2db87e771bb
Chris Little <chrisclittle@gmail.com>
2015-05-11 14:45:40 -0700

    pylint score as a float would be better

--- badge_update.py
+++ badge_update.py
@@ -13,8 +13,8 @@ if not os.path.isfile('./README.rst'):
 if not os.path.isfile('./pylint.log'):
     exit('Please direct Pylint output to pylint.log')
 pylint_text = open('pylint.log', 'r', encoding='utf-8').read()
-pylint_score = re.search('Your code has been rated at ([0-9\.]+)',
-                         pylint_text).group(1)
+pylint_score = float(re.search('Your code has been rated at ([0-9\.]+)',
+                               pylint_text).group(1))
 
 if not os.path.isfile('./pep8.log'):
     exit('Please direct PEP8 output to pep8.log')
@@ -24,7 +24,7 @@ pep8_score = sum([int(n) for n in re.findall('\n([0-9]+)  +', pep8_text)])
 readme_text = open('README.rst', 'r', encoding='utf-8').read()
 prefix = 'https://img.shields.io/badge/Pylint-'
 readme_text = re.sub(prefix+'([0-9\.]+)',
-                     prefix+pylint_score, readme_text, 1)
+                     prefix+str(pylint_score), readme_text, 1)
 prefix = 'https://img.shields.io/badge/PEP8-'
 readme_text = re.sub(prefix+'([0-9\.]+)',
                      prefix+str(pep8_score), readme_text, 1)
