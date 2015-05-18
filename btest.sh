741d513240d22456feef5adafdb04abd01c03a90
Chris Little <chrisclittle@gmail.com>
2015-05-17 17:16:01 -0700

    added quick_mode

--- btest.sh
+++ btest.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+quick_mode=0
+while [ "$1" != "" ]; do
+    case $1 in
+        -q | --quick ) quick_mode=1
+    esac
+    shift
+done
+
 ./cleanup.sh
 
 python setup.py build
@@ -11,11 +19,13 @@ sudo python3 setup.py bdist_wheel
 sudo python setup.py install
 sudo python3 setup.py install
 
-nosetests -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
-nosetests3 -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
+if [ "$quick_mode" = "0" ]; then
+    nosetests -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
+    nosetests3 -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=abydos .
 
-pylint --rcfile=pylint.rc abydos > pylint.log
-pep8 -v --statistics --exclude=.git,__pycache__,build,_bmdata.py,docs . > pep8.log
+    pylint --rcfile=pylint.rc abydos > pylint.log
+    pep8 -v --statistics --exclude=.git,__pycache__,build,_bmdata.py,docs . > pep8.log
+fi
 
 ./badge_update.py
 
