54901ec67f9ef30a6e5f58f5d380639c663caca7
Chris Little <chrisclittle@gmail.com>
2018-07-29 19:29:35 -0700

    flake8 corrections

--- badge_update.py
+++ badge_update.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Copyright 2015-2018 by Christopher C. Little.
+# This file is part of Abydos.
+#
+# Abydos is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Abydos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Abydos. If not, see <http://www.gnu.org/licenses/>.
+
+
 import os.path
 import re
 
@@ -9,6 +26,7 @@ badge_colors = ('brightgreen', 'green', 'yellowgreen', 'yellow',
 
 
 def pylint_color(score):
+    """Return Pylint badge color."""
     # These are the score cutoffs for each color above.
     # I.e. score==10 -> brightgreen, down to 7.5 > score >= 5 -> orange
     score_cutoffs = (10, 9.5, 8.5, 7.5, 5)
@@ -20,6 +38,7 @@ def pylint_color(score):
 
 
 def pycodestyle_color(score):
+    """Return pycodestyle badge color."""
     # These are the score cutoffs for each color above.
     # I.e. score==0 -> brightgreen, down to 100 < score <= 200 -> orange
     score_cutoffs = (0, 20, 50, 100, 200)
@@ -31,6 +50,7 @@ def pycodestyle_color(score):
 
 
 def flake8_color(score):
+    """Return flake8 badge color."""
     # These are the score cutoffs for each color above.
     # I.e. score==0 -> brightgreen, down to 100 < score <= 200 -> orange
     score_cutoffs = (0, 20, 50, 100, 200)
@@ -53,11 +73,13 @@ pylint_score = max(float(re.search('Your code has been rated at (\-?[0-9\.]+)',
 if not os.path.isfile('./pycodestyle.log'):
     exit('Please direct pycodestyle output to pycodestyle.log')
 pycodestyle_text = open('pycodestyle.log', 'r', encoding='utf-8').read()
-pycodestyle_score = sum([int(n) for n in re.findall('\n([0-9]+)  +', pycodestyle_text)])
+pycodestyle_score = sum(int(n) for n in re.findall('\n([0-9]+)  +',
+                                                   pycodestyle_text))
 
 if not os.path.isfile('./flake8.log'):
     exit('Please direct flake8 output to flake8.log')
-flake8_score = int(open('flake8.log', 'r', encoding='utf-8').read().split()[-1])
+flake8_score = int(open('flake8.log', 'r', encoding='utf-8').read()
+                   .split()[-1])
 
 readme_text = open('README.rst', 'r', encoding='utf-8').read()
 
--- setup.py
+++ setup.py
@@ -1,16 +1,39 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+# Copyright 2014-2018 by Christopher C. Little.
+# This file is part of Abydos.
+#
+# Abydos is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Abydos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Abydos. If not, see <http://www.gnu.org/licenses/>.
+
+"""setup.py
+
+setuptools configuration file for Abydos
+"""
+
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+from setuptools import find_packages, setup
+
+HERE = path.abspath(path.dirname(__file__))
 
 
 def readfile(fn):
     """Read fn and return the contents."""
-    with open(path.join(here, fn), 'r', encoding='utf-8') as f:
+    with open(path.join(HERE, fn), 'r', encoding='utf-8') as f:
         return f.read()
+
 
 setup(
     name='abydos',
