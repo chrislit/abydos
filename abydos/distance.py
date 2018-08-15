11708c9dbcb4ec41df9168a82991ddb3922dbe75
Chris Little <chrisclittle@gmail.com>
2018-08-15 15:23:35 -0700

    added common Sift4

--- abydos/distance.py
+++ abydos/distance.py
@@ -3090,6 +3090,90 @@ def sift4_simplest(src, tar, max_offset=0):
     return round(max(src_len, tar_len) - lcss)
 
 
+def sift4_common(src, tar, max_offset=0, max_distance=0):
+    """Return the "common" Sift4 distance between two terms.
+
+    This is an approximation of edit distance, described in:
+    Zackwehdex, Siderite. 2014. "Super Fast and Accurate string distance
+    algorithm: Sift4."
+    https://siderite.blogspot.com/2014/11/super-fast-and-accurate-string-distance.html
+
+    :param str src, tar: two strings to be compared
+    :param max_offset: the number of characters to search for matching letters
+    :param max_distance: the distance at which to stop and exit
+    :return:
+    """
+    if not src:
+        return len(tar)
+
+    if not tar:
+        return len(src)
+
+    src_len = len(src)
+    tar_len = len(tar)
+
+    src_cur = 0
+    tar_cur = 0
+    lcss = 0
+    local_cs = 0
+    trans = 0
+    offset_arr = []
+
+    while (src_cur < src_len) and (tar_cur < tar_len):
+        if src[src_cur] == tar[tar_cur]:
+            local_cs += 1
+            is_trans = False
+            i = 0
+            while i < len(offset_arr):
+                ofs = offset_arr[i]
+                if src_cur <= ofs['src_cur'] or tar_cur <= ofs['tar_cur']:
+                    is_trans = (abs(tar_cur-src_cur) >= abs(ofs['tar_cur']-ofs['src_cur']))
+                    if is_trans:
+                        trans += 1
+                    elif not ofs['trans']:
+                        ofs['trans'] = True
+                        trans += 1
+                    break
+                elif src_cur > ofs['tar_cur'] and tar_cur > ofs['src_cur']:
+                    del offset_arr[i]
+                else:
+                    i += 1
+
+            offset_arr.append({'src_cur': src_cur, 'tar_cur': tar_cur, 'trans': is_trans})
+        else:
+            lcss += local_cs
+            local_cs = 0
+            if src_cur != tar_cur:
+                src_cur = tar_cur = min(src_cur, tar_cur)
+            for i in range(max_offset):
+                if not ((src_cur+i < src_len) or (tar_cur+i < tar_len)):
+                    break
+                if (src_cur+i < src_len) and (src[src_cur+i] == tar[tar_cur]):
+                    src_cur += i-1
+                    tar_cur -= 1
+                    break
+                if (tar_cur+i < tar_len) and (src[src_cur] == tar[tar_cur+i]):
+                    src_cur -= 1
+                    tar_cur += i-1
+                    break
+
+        src_cur += 1
+        tar_cur += 1
+
+        if max_distance:
+            temporary_distance = max(src_cur, tar_cur) - lcss + trans
+            if temporary_distance >= max_distance:
+                return round(temporary_distance)
+
+        if (src_cur >= src_len) or (tar_cur >= tar_len):
+            lcss += local_cs
+            local_cs = 0
+            src_cur = tar_cur = min(src_cur, tar_cur)
+
+    lcss += local_cs
+    return round(max(src_len, tar_len) - lcss + trans)
+
+
 def sim_tfidf(src, tar, qval=2, docs_src=None, docs_tar=None):
     """Return the TF-IDF similarity of two strings.
 
