11338c02ed0bb0a5f664c50fb0a2e310f7df34de
Chris Little <chrisclittle@gmail.com>
2018-09-12 11:58:46 -0700

    added Schinke stemmer

--- abydos/stemmer.py
+++ abydos/stemmer.py
@@ -2243,6 +2243,89 @@ def paice_husk(word):
     return word
 
 
+def schinke(word, modified=False):
+    """Return the stem of a word according to the Schinke stemmer.
+
+    :param word:
+    :return:
+    """
+    word = unicodedata.normalize('NFKD', text_type(word.lower()))
+    word = ''.join(c for c in word if c in
+                   {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                    'y', 'z'})
+
+    noun = word
+    verb = word
+
+    # Rule 2
+    word = word.replace('j', 'i').replace('v', 'u')
+
+    # Rule 3
+    keep_que = {'at', 'quo', 'ne', 'ita', 'abs', 'aps', 'abus', 'adae', 'adus',
+                'deni', 'de', 'sus', 'obli', 'perae', 'plenis', 'quando',
+                'quis', 'quae', 'cuius', 'cui', 'quem', 'quam', 'qua', 'qui',
+                'quorum', 'quarum', 'quibus', 'quos' 'quas', 'quotusquis',
+                'quous', 'ubi', 'undi', 'us', 'uter', 'uti' 'utro', 'utribi',
+                'tor', 'co', 'conco', 'contor', 'detor', 'deco', 'exco',
+                'extor', 'obtor', 'optor', 'retor', 'reco', 'attor', 'inco',
+                'intor', 'praetor'}
+    if word[-3:] == 'que':
+        if word[:-3] in keep_que:
+            return {'n': word, 'v': word}
+        else:
+            word = word[:-3]
+
+    # Rule 4
+    n_endings = {4: {'ibus'},
+               3: {'ius'},
+               2: {'is', 'nt', 'ae', 'os', 'am', 'ud', 'as', 'um', 'em', 'us',
+                   'es', 'ia'},
+               1: {'a', 'e', 'i', 'o', 'u'}}
+    for endlen in range(4, 0, -1):
+        if word[-endlen:] in n_endings[endlen]:
+            if len(word)-2 >= endlen:
+                noun = word[:-endlen]
+            else:
+                noun = word
+            break
+
+    v_endings_strip = {6: {},
+                       5: {},
+                       4: {'mini', 'ntur', 'stis'},
+                       3: {'mur', 'mus', 'ris', 'sti', 'tis', 'tur'},
+                       2: {'ns', 'nt', 'ri'},
+                       1: {'m', 'r', 's', 't'}}
+    v_endings_alter = {6: {'iuntur'},
+                       5: {'beris', 'erunt', 'untur'},
+                       4: {'iunt'},
+                       3: {'bor', 'ero', 'unt'},
+                       2: {'bo'},
+                       1: {}}
+    for endlen in range(6, 0, -1):
+        if word[-endlen:] in v_endings_strip[endlen]:
+            if len(word)-2 >= endlen:
+                verb = word[:-endlen]
+            else:
+                verb = word
+            break
+        if word[-endlen:] in v_endings_alter[endlen]:
+            if word[-endlen:] in {'iuntur', 'erunt', 'untur', 'iunt', 'unt'}:
+                new_word = word[:-endlen]+'i'
+            elif word[-endlen:] in {'beris', 'bor', 'bo'}:
+                new_word = word[:-endlen]+'bi'
+            else:
+                new_word = word[:-endlen]+'eri'
+
+            if len(new_word) >= 2:
+                verb = new_word
+            else:
+                verb = word
+            break
+
+    return {'n': noun, 'v': verb}
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
