edc501942c39be08f3244c5e0a14ecfd572ee67b
Chris Little <chrisclittle@gmail.com>
2018-09-14 21:56:25 -0700

    added docs-only build option (--docs or -d)

--- btest.sh
+++ btest.sh
@@ -2,12 +2,17 @@
 
 quick_mode=0
 clean=0
+docs_only=0
+
 while [ "$1" != "" ]; do
     case $1 in
         -q | --quick | -qc) quick_mode=1
     esac
     case $1 in
 	-c | --clean | -qc) clean=1
+    esac
+    case $1 in
+	-d | --docs) docs_only=1
     esac
     shift
 done
@@ -16,21 +21,23 @@ if [ "$clean" = "1" ]; then
     ./cleanup.sh
 fi
 
-python setup.py build
+if [ "docs_only" = "0" ]; then
+    python setup.py build
 
-python setup.py sdist
-python setup.py bdist_wheel
+    python setup.py sdist
+    python setup.py bdist_wheel
 
-python setup.py install
+    python setup.py install
 
-if [ "$quick_mode" = "0" ]; then
-    nosetests .
+    if [ "$quick_mode" = "0" ]; then
+	nosetests .
 
-    pylint --rcfile=pylint.rc abydos > pylint.log
-    pycodestyle -v --statistics --exclude=.git,__pycache__,build,dist,docs . > pycodestyle.log
-    flake8 . > flake8.log
+	pylint --rcfile=pylint.rc abydos > pylint.log
+	pycodestyle -v --statistics --exclude=.git,__pycache__,build,dist,docs . > pycodestyle.log
+	flake8 . > flake8.log
 
-    ./badge_update.py
+	./badge_update.py
+    fi
 fi
 
 sphinx-apidoc -e -f -M -o docs ./abydos
