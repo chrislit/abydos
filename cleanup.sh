d0af25c4ff6331a00ed412eb7dfcee7316909d6b
Chris Little <chrisclittle@gmail.com>
2018-09-19 14:54:11 -0700

    added more directories to delete

--- cleanup.sh
+++ cleanup.sh
@@ -5,6 +5,9 @@ rm -rf ./flake8
 rm -rf ./abydos.egg-info
 rm -rf ./dist
 rm -rf ./build
+rm -rf ./.tox
+rm -rf ./.mypy_cache
+rm -rf ./docs/_build/*
 rm -rf ./*.log
 rm -rf ./.coverage
 rm -rf */*.pyc
