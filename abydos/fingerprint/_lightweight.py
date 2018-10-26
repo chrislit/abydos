63d8364be3c8cc724dd45a5b17f4576a883d028f
Chris Little <chrisclittle@gmail.com>
2018-10-25 23:19:21 -0700

    refactored fingerprint API

--- abydos/fingerprint/_lightweight.py
+++ abydos/fingerprint/_lightweight.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Abydos. If not, see <http://www.gnu.org/licenses/>.
 
-"""abydos.fingerprint.lightweight.
+"""abydos.fingerprint._lightweight.
 
 The fingerprint.lightweight module implements string fingerprints developed
 by Cisłak & Grabowski in :cite:`Cislak:2017`:
--- abydos/fingerprint/_synoname.py
+++ abydos/fingerprint/_synoname.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Abydos. If not, see <http://www.gnu.org/licenses/>.
 
-"""abydos.fingerprint.
+"""abydos.fingerprint._synoname
 
 The fingerprint.synoname module implements the Synoname toolcode.
 """
