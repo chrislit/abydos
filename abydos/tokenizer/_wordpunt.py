d3ba36dbefe317d48b65016a940fe1c532f6ebd7
Chris Little <chrisclittle@gmail.com>
2018-11-23 20:06:24 -0800

    added whitespace & wordpunct tokenizers

---
+++ abydos/tokenizer/_wordpunt.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2014-2018 by Christopher C. Little.
+# This file is part of Abydos.
+#
+# Abydos is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Abydos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Abydos. If not, see <http://www.gnu.org/licenses/>.
+
+"""abydos.tokenizer._wordpunct.
+
+Wordpunct tokenizer (analogous to NLTK's workpunct tokenizer)
+"""
+
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+import re
+
+from ._tokenizer import _Tokenizer
+
+__all__ = ['Wordpunct']
+
+
+class Wordpunct(_Tokenizer):
+    """A wordpunct tokenizer
+
+    .. versionadded:: 0.4.0
+    """
+
+    def __init__(
+        self, string='', scaler=None
+    ):
+        """Initialize tokenizer.
+
+        Parameters
+        ----------
+        string : str
+            A string to extract q-grams from
+        scaler : None, str, or function
+
+
+        Examples
+        --------
+
+        .. versionadded:: 0.4.0
+
+        """
+        super(Whitespace, self).__init__(scaler)
+
+        # Save parameters
+        self.string = string
+
+        self._ordered_list = []
+
+        self.tokenize(self.string)
+
+    def tokenize(self, string):
+        """Tokenize the term and store it.
+
+        The tokenized term is stored as an ordered list and as a Counter
+        object.
+
+        Args
+        ----
+        string : str
+            The string to tokenize
+
+        .. versionadded:: 0.4.0
+
+        """
+        self.string = string
+
+        self._ordered_list = re.findall(r'\w+|[^\w\s]+', self.string)
+
+        super(_Tokenizer, self).__init__(self._ordered_list)
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
