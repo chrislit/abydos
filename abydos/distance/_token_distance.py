446ded1f5031e60d357b22149aa10b92bd4b3b78
Chris Little <chrisclittle@gmail.com>
2018-12-15 14:14:30 -0800

    moved intersection methods into _token_distance

--- abydos/distance/_token_distance.py
+++ abydos/distance/_token_distance.py
@@ -18,7 +18,8 @@
 
 """abydos.distance._token_distance.
 
-The distance._TokenDistance module implements abstract class _TokenDistance.
+The distance._token_distance._TokenDistance module implements abstract class
+_TokenDistance.
 """
 
 from __future__ import (
@@ -29,7 +30,11 @@ from __future__ import (
 )
 
 from collections import Counter
+from itertools import product
 
+from ._damerau_levenshtein import DamerauLevenshtein
+from ._lcprefix import LCPrefix
+from ._levenshtein import Levenshtein
 from ._distance import _Distance
 from ..tokenizer import QGrams, WhitespaceTokenizer
 
@@ -40,7 +45,7 @@ class _TokenDistance(_Distance):
     .. versionadded:: 0.3.6
     """
 
-    def __init__(self, tokenizer=None, **kwargs):
+    def __init__(self, tokenizer=None, intersection_type='crisp', **kwargs):
         """Initialize _TokenDistance instance.
 
         Parameters
@@ -76,6 +81,20 @@ class _TokenDistance(_Distance):
                 self.params['alphabet'] = len(self.params['alphabet'])
         else:
             self.params['alphabet'] = None
+
+        if intersection_type == 'soft':
+            if 'metric' not in self.params or self.params['metric'] is None:
+                self.params['metric'] = DamerauLevenshtein()
+            self._lcprefix = LCPrefix()
+            self.intersection = self._soft_intersection
+        elif intersection_type == 'fuzzy':
+            if 'metric' not in self.params or self.params['metric'] is None:
+                self.params['metric'] = Levenshtein()
+            if 'threshold' not in self.params:
+                self.params['threshold'] = 0.8
+            self.intersection = self._fuzzy_intersection
+        else:
+            self.intersection = self._crisp_intersection
 
         self._src_tokens = Counter()
         self._tar_tokens = Counter()
@@ -127,27 +146,48 @@ class _TokenDistance(_Distance):
         return self._src_tokens, self._tar_tokens
 
     def src_only(self):
-        """Return the src tokens minus the tar tokens."""
-        return self._src_tokens - self._tar_tokens
+        """Return the src tokens minus the tar tokens.
+
+        For (multi-)sets S and T, this is :math:`S \setminus T`.
+        """
+        return self._src_tokens - self.intersection()
 
     def tar_only(self):
-        """Return the tar tokens minus the src tokens."""
-        return self._tar_tokens - self._src_tokens
+        """Return the tar tokens minus the src tokens.
+
+        For (multi-)sets S and T, this is :math:`T \setminus S`.
+        """
+        return self._tar_tokens - self.intersection()
 
     def symmetric_difference(self):
-        """Return the symmetric difference of tokens from src and tar."""
+        """Return the symmetric difference of tokens from src and tar.
+
+        For (multi-)sets S and T, this is :math:`S \triangle T`.
+        """
         return self.src_only() + self.tar_only()
 
     def total(self):
-        """Return the sum of the sets."""
+        """Return the sum of the sets.
+
+        For (multi-)sets S and T, this is :math:`S + T`.
+
+        In the case of multisets, this counts values in the interesection
+        twice. In the case of sets, this is identical to the union.
+        """
         return self._src_tokens + self._tar_tokens
 
     def union(self):
-        """Return the union of tokens from src and tar."""
+        """Return the union of tokens from src and tar.
+
+        For (multi-)sets S and T, this is :math:`S \cup T`.
+        """
         return self.total() - self.intersection()
 
-    def intersection(self):
-        """Return the intersection of tokens from src and tar."""
+    def _crisp_intersection(self):
+        """Return the intersection of tokens from src and tar.
+
+        For (multi-)sets S and T, this is :math:`S \cap T`.
+        """
         return self._src_tokens & self._tar_tokens
 
     def difference(self):
@@ -156,6 +196,44 @@ class _TokenDistance(_Distance):
         _src_copy.subtract(self._tar_tokens)
         return _src_copy
 
+    def _soft_intersection(self):
+        """Return the soft intersection of the tokens in src and tar."""
+        intersection = sum(self._crisp_intersection().values())
+
+        src_only = self.src_only()
+        tar_only = self.tar_only()
+
+        def _membership(src, tar):
+            greater_length = max(len(src), len(tar))
+            return max(greater_length - self.params['metric'].dist_abs(src, tar),
+                       self._lcprefix.dist_abs(src, tar)) / greater_length
+
+        memberships = {(src, tar): _membership(src, tar) for src, tar in product(self.src_only().items(), self.tar_only().items())}
+        while memberships:
+            src_tok, tar_tok = max(memberships, key=memberships.get)
+            if memberships[src_tok, tar_tok] == 0.0:
+                break
+            pairings = min(src_only[src_tok], tar_only[tar_tok])
+            intersection += memberships[src_tok, tar_tok]*pairings
+            src_only[src_tok] -= pairings
+            tar_only[tar_tok] -= pairings
+            del memberships[src_tok, tar_tok]
+
+        return intersection
+
+    def _fuzzy_intersection(self):
+        """Return the fuzzy intersection of the tokens in src and tar."""
+        overlap = sum(self._crisp_intersection().values())
+        src_only = self.src_only()
+        tar_only = self.tar_only()
+
+        for src_tok in src_only:
+            for tar_tok in tar_only:
+                sim = self.params['metric'].sim(src_tok, tar_tok)
+                if sim >= self.params['threshold']:
+                    overlap += sim
+
+        return overlap
 
 if __name__ == '__main__':
     import doctest
