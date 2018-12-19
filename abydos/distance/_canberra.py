f21093c7f361a88e5b0896ee97b77650403e6467
Chris Little <chrisclittle@gmail.com>
2018-12-19 15:55:42 -0800

    added Canberra

--- abydos/distance/_canberra.py
+++ abydos/distance/_canberra.py
@@ -36,14 +36,14 @@ __all__ = ['Canberra']
 class Canberra(_TokenDistance):
     r"""Canberra distance.
 
-    For two sets X and Y,
+    For two sets X and Y, the Canberra distance :cite:`Lance:1966,Lance:1967b`
+    is :math:`sim_{Canberra}(X, Y) = \frac{|X \triangle Y|}{|X|+|Y|}`.
 
     .. versionadded:: 0.4.0
     """
 
     def __init__(
         self,
-        alphabet=None,
         tokenizer=None,
         intersection_type='crisp',
         **kwargs
@@ -52,25 +52,6 @@ class Canberra(_TokenDistance):
 
         Parameters
         ----------
-        alphabet : Counter, collection, int, or None
-            This represents the alphabet of possible tokens.
-
-                - If a Counter is supplied, it is used directly in computing
-                  the complement of the tokens in both sets.
-                - If a collection is supplied, it is converted to a Counter
-                  and used directly. In the case of a single string being
-                  supplied and the QGram tokenizer being used, the full
-                  alphabet is inferred (i.e.
-                  :math:`len(set(alphabet+QGrams.start_stop))^{QGrams.qval}` is
-                  used as the cardinality of the full alphabet.
-                - If an int is supplied, it is used as the cardinality of the
-                  full alphabet.
-                - If None is supplied, the cardinality of the full alphabet
-                  is inferred if QGram tokenization is used (i.e.
-                  :math:`28^{QGrams.qval}` is used as the cardinality of the
-                  full alphabet or :math:`26` if QGrams.qval is 1, which
-                  assumes the strings are English language strings). Otherwise,
-                  The cardinality of the complement of the total will be 0.
         tokenizer : _Tokenizer
             A tokenizer instance from the abydos.tokenizer package
         intersection_type : str
@@ -108,7 +89,6 @@ class Canberra(_TokenDistance):
 
         """
         super(Canberra, self).__init__(
-            alphabet=alphabet,
             tokenizer=tokenizer,
             intersection_type=intersection_type,
             **kwargs
@@ -147,13 +127,7 @@ class Canberra(_TokenDistance):
         """
         self.tokenize(src, tar)
 
-        # a = self.intersection_card()
-        # b = self.src_only_card()
-        # c = self.tar_only_card()
-        # d = self.total_complement_card()
-        # n = self.population_card()
-
-        return 0.0
+        return self.symmetric_difference_card()/self.total_card()
 
 
 if __name__ == '__main__':
