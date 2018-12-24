01e23a5802a47644b0e512b543589bc9c50adec4
Chris Little <chrisclittle@gmail.com>
2018-12-23 21:21:42 -0800

    added Johnson similarity

--- abydos/distance/_johnson.py
+++ abydos/distance/_johnson.py
@@ -36,14 +36,16 @@ __all__ = ['Johnson']
 class Johnson(_TokenDistance):
     r"""Johnson similarity.
 
-    For two sets X and Y,
+    For two sets X and Y, the Johnson
+    similarity :cite:`Johnson:1967` is
+    :math:`sim_{Johnson}(X, Y) =
+    \frac{(|X \cap Y|}{|X|} + \frac{|Y \cap X}{|Y|}`.
 
     .. versionadded:: 0.4.0
     """
 
     def __init__(
         self,
-        alphabet=None,
         tokenizer=None,
         intersection_type='crisp',
         **kwargs
@@ -52,25 +54,6 @@ class Johnson(_TokenDistance):
 
         Parameters
         ----------
-        alphabet : Counter, collection, int, or None
-            This represents the alphabet of possible tokens.
-
-                - If a Counter is supplied, it is used directly in computing
-                  the complement of the tokens in both sets.
-                - If a collection is supplied, it is converted to a Counter
-                  and used directly. In the case of a single string being
-                  supplied and the QGram tokenizer being used, the full
-                  alphabet is inferred (i.e.
-                  :math:`len(set(alphabet+QGrams.start_stop))^{QGrams.qval}` is
-                  used as the cardinality of the full alphabet.
-                - If an int is supplied, it is used as the cardinality of the
-                  full alphabet.
-                - If None is supplied, the cardinality of the full alphabet
-                  is inferred if QGram tokenization is used (i.e.
-                  :math:`28^{QGrams.qval}` is used as the cardinality of the
-                  full alphabet or :math:`26` if QGrams.qval is 1, which
-                  assumes the strings are English language strings). Otherwise,
-                  The cardinality of the complement of the total will be 0.
         tokenizer : _Tokenizer
             A tokenizer instance from the abydos.tokenizer package
         intersection_type : str
@@ -108,7 +91,6 @@ class Johnson(_TokenDistance):
 
         """
         super(Johnson, self).__init__(
-            alphabet=alphabet,
             tokenizer=tokenizer,
             intersection_type=intersection_type,
             **kwargs
@@ -147,13 +129,11 @@ class Johnson(_TokenDistance):
         """
         self.tokenize(src, tar)
 
-        # a = self.intersection_card()
-        # b = self.src_only_card()
-        # c = self.tar_only_card()
-        # d = self.total_complement_card()
-        # n = self.population_card()
+        a = self.intersection_card()
+        ab = self.src_card()
+        ac = self.tar_card()
 
-        return 0.0
+        return a/ab+a/ac
 
 
 if __name__ == '__main__':
