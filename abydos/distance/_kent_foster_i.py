8e6c15cbdf1d9a6357a6f976cf2dd1c7132523c9
Chris Little <chrisclittle@gmail.com>
2019-01-08 02:36:40 -0800

    added 3 similarities & 4 refs

--- abydos/distance/_kent_foster_i.py
+++ abydos/distance/_kent_foster_i.py
@@ -37,11 +37,18 @@ class KentFosterI(_TokenDistance):
     r"""Kent & Foster I similarity.
 
     For two sets X and Y and a population N, Kent & Foster I similarity
-    :cite:`CITATION` is
+    :cite:`Kent:1977`, :math:`K_{occ}`, is
 
         .. math::
 
             sim_{KentFosterI}(X, Y) =
+            \frac{|X \cap Y| - \frac{|X|\cdot|Y|}{|X \cup Y|}}
+            {|X \cap Y| - \frac{|X|\cdot|Y|}{|X \cup Y|} +
+            |X \setminus Y| + |Y \setminus X|}
+
+    Kent & Foster derived this from Cohen's :math:`\kappa` by "subtracting
+    appropriate chance agreement correction figures from the numerators and
+    denominators" to arrive at an occurrence reliability measure.
 
     In :ref:`2x2 confusion table terms <confusion_table>`, where a+b+c+d=n,
     this is
@@ -49,6 +56,7 @@ class KentFosterI(_TokenDistance):
         .. math::
 
             sim_{KentFosterI} =
+            \frac{a-\frac{(a+b)(a+c)}{a+b+c}}{a-\frac{(a+b)(a+c)}{a+b+c}+b+c}
 
     .. versionadded:: 0.4.0
     """
@@ -134,13 +142,13 @@ class KentFosterI(_TokenDistance):
         """
         self.tokenize(src, tar)
 
-        # a = self.intersection_card()
-        # b = self.src_only_card()
-        # c = self.tar_only_card()
-        # d = self.total_complement_card()
-        # n = self.population_card()
+        a = self.intersection_card()
+        b = self.src_only_card()
+        c = self.tar_only_card()
 
-        return 0.0
+        bigterm = a-((a+b)*(a+c)/(a+b+c))
+
+        return bigterm/(bigterm+b+c)
 
 
 if __name__ == '__main__':
