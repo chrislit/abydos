7dc6a150330947752341c2d8138b4106b776d44d
Chris Little <chrisclittle@gmail.com>
2019-02-02 04:12:39 -0800

    added n Choose r helper

---
+++ abydos/util/_ncr.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 by Christopher C. Little.
+# This file is part of Abydos.
+#
+# Abydos is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Abydos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Abydos. If not, see <http://www.gnu.org/licenses/>.
+
+"""abydos.util._ncr.
+
+The util._ncr module defines ncr, which computes n Choose r.
+"""
+
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+from math import factorial
+
+__all__ = []
+
+
+def _ncr(n, r):
+    r"""Return n Choose r.
+
+    Cf. https://en.wikipedia.org/wiki/Combination
+
+    Parameters
+    ----------
+    n : int
+        The number of elements in the set/multiset
+    r : int
+        The number of elements to choose
+
+    Returns
+    -------
+    numeric
+        n Choose r
+
+    Examples
+    --------
+    >>> _ncr(4, 2)
+    6
+    >>> _ncr(10, 3)
+    120
+
+    .. versionadded:: 0.4.0
+
+    """
+    return factorial(n)/(factorial(r)*factorial(n-r))
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
