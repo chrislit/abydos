554ddd7551ea43cbf2be89d6e4c5837a66763a1c
Chris Little <chrisclittle@gmail.com>
2019-02-08 14:20:00 -0800

    added positional q-gram jaccard/dice/overlap

--- abydos/distance/_positional_q_gram_dice.py
+++ abydos/distance/_positional_q_gram_dice.py
@@ -18,7 +18,7 @@
 
 """abydos.distance._positional_q_gram_dice.
 
-Positional Q-Gram Dice distance
+Positional Q-Gram Dice coefficient
 """
 
 from __future__ import (
@@ -28,35 +28,62 @@ from __future__ import (
     unicode_literals,
 )
 
+from collections import defaultdict
+
 from ._distance import _Distance
+from ..tokenizer import QGrams, WhitespaceTokenizer
 
 __all__ = ['PositionalQGramDice']
 
 
 class PositionalQGramDice(_Distance):
-    r"""Positional Q-Gram Dice distance.
+    r"""Positional Q-Gram Dice coefficient.
 
-    Positional Q-Gram Dice distance :cite:`CITATION`
+    Positional Q-Gram Dice coefficient :cite:`Gravano:2001,Christen:2006`
 
     .. versionadded:: 0.4.0
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, max_dist=1, tokenizer=None, **kwargs):
         """Initialize PositionalQGramDice instance.
 
         Parameters
         ----------
+        max_dist : int
+            The maximum positional distance between to q-grams to count as a
+            match.
+        tokenizer : _Tokenizer
+            A tokenizer instance from the :py:mod:`abydos.tokenizer` package
         **kwargs
             Arbitrary keyword arguments
+
+        Other Parameters
+        ----------------
+        qval : int
+            The length of each q-gram. Using this parameter and tokenizer=None
+            will cause the instance to use the QGram tokenizer with this
+            q value.
 
 
         .. versionadded:: 0.4.0
 
         """
-        super(PositionalQGramDice, self).__init__(**kwargs)
+        super(PositionalQGramDice, self).__init__(
+            tokenizer=tokenizer, **kwargs
+        )
+        self._max_dist = max_dist
+
+        qval = 2 if 'qval' not in self.params else self.params['qval']
+        self.params['tokenizer'] = (
+            tokenizer
+            if tokenizer is not None
+            else WhitespaceTokenizer()
+            if qval == 0
+            else QGrams(qval=qval, start_stop='$#', skip=0, scaler=None)
+        )
 
     def dist(self, src, tar):
-        """Return the Positional Q-Gram Dice distance of two strings.
+        """Return the Positional Q-Gram Dice coefficient of two strings.
 
         Parameters
         ----------
@@ -68,7 +95,7 @@ class PositionalQGramDice(_Distance):
         Returns
         -------
         float
-            Positional Q-Gram Dice distance
+            Positional Q-Gram Dice coefficient
 
         Examples
         --------
@@ -86,8 +113,38 @@ class PositionalQGramDice(_Distance):
         .. versionadded:: 0.4.0
 
         """
+        src_list = self.params['tokenizer'].tokenize(src).get_list()
+        tar_list = self.params['tokenizer'].tokenize(tar).get_list()
 
-        return 0.0
+        src_pos = defaultdict(list)
+        tar_pos = defaultdict(list)
+
+        intersection = 0
+
+        for pos in range(len(src_list)):
+            src_pos[src_list[pos]].append(pos)
+        for pos in range(len(tar_list)):
+            tar_pos[tar_list[pos]].append(pos)
+
+        src_matched = []
+        tar_matched = []
+
+        for tok in src_pos:
+            if tok in tar_pos:
+                for sp in src_pos[tok]:
+                    for tp in tar_pos[tok]:
+                        if (
+                            abs(sp - tp) <= self._max_dist
+                            and sp not in src_matched
+                            and tp not in tar_matched
+                        ):
+                            intersection += 1
+                            src_matched.append(sp)
+                            tar_matched.append(tp)
+
+        denom = len(src_list) + len(tar_list)
+
+        return 2 * intersection / denom
 
 
 if __name__ == '__main__':
