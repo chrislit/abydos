3d7904f6a5c4b25b677a5f463221790b9c09ce67
Chris Little <chrisclittle@gmail.com>
2019-03-01 15:16:55 -0800

    some progress on BLEU corner cases

--- tests/distance/test_distance_bleu.py
+++ tests/distance/test_distance_bleu.py
@@ -46,16 +46,16 @@ class BLEUTestCases(unittest.TestCase):
         # Base cases
         self.assertEqual(self.cmp.sim('', ''), float('nan'))
         self.assertEqual(self.cmp.sim('a', ''), float('nan'))
-        self.assertEqual(self.cmp.sim('', 'a'), float('nan'))
+        self.assertEqual(self.cmp.sim('', 'a'), 2.718281828459045)
         self.assertEqual(self.cmp.sim('abc', ''), float('nan'))
-        self.assertEqual(self.cmp.sim('', 'abc'), float('nan'))
-        self.assertEqual(self.cmp.sim('abc', 'abc'), float('nan'))
-        self.assertEqual(self.cmp.sim('abcd', 'efgh'), float('nan'))
+        self.assertEqual(self.cmp.sim('', 'abc'), 2.718281828459045)
+        self.assertEqual(self.cmp.sim('abc', 'abc'), 1.0)
+        self.assertEqual(self.cmp.sim('abcd', 'efgh'), 1.0)
 
-        self.assertAlmostEqual(self.cmp.sim('Nigel', 'Niall'), float('nan'))
-        self.assertAlmostEqual(self.cmp.sim('Niall', 'Nigel'), float('nan'))
-        self.assertAlmostEqual(self.cmp.sim('Colin', 'Coiln'), float('nan'))
-        self.assertAlmostEqual(self.cmp.sim('Coiln', 'Colin'), float('nan'))
+        self.assertAlmostEqual(self.cmp.sim('Nigel', 'Niall'), 0.6223329773)
+        self.assertAlmostEqual(self.cmp.sim('Niall', 'Nigel'), 0.6223329773)
+        self.assertAlmostEqual(self.cmp.sim('Colin', 'Coiln'), 0.7071067812)
+        self.assertAlmostEqual(self.cmp.sim('Coiln', 'Colin'), 0.7071067812)
         self.assertAlmostEqual(
             self.cmp.sim('ATCAACGAGT', 'AACGATTAG'), 0.5119598032
         )
@@ -65,16 +65,16 @@ class BLEUTestCases(unittest.TestCase):
         # Base cases
         self.assertEqual(self.cmp.dist('', ''), float('nan'))
         self.assertEqual(self.cmp.dist('a', ''), float('nan'))
-        self.assertEqual(self.cmp.dist('', 'a'), float('nan'))
+        self.assertEqual(self.cmp.dist('', 'a'), -1.718281828459045)
         self.assertEqual(self.cmp.dist('abc', ''), float('nan'))
-        self.assertEqual(self.cmp.dist('', 'abc'), float('nan'))
-        self.assertEqual(self.cmp.dist('abc', 'abc'), float('nan'))
-        self.assertEqual(self.cmp.dist('abcd', 'efgh'), float('nan'))
+        self.assertEqual(self.cmp.dist('', 'abc'), -1.718281828459045)
+        self.assertEqual(self.cmp.dist('abc', 'abc'), 0.0)
+        self.assertEqual(self.cmp.dist('abcd', 'efgh'), 0.0)
 
-        self.assertAlmostEqual(self.cmp.dist('Nigel', 'Niall'), float('nan'))
-        self.assertAlmostEqual(self.cmp.dist('Niall', 'Nigel'), float('nan'))
-        self.assertAlmostEqual(self.cmp.dist('Colin', 'Coiln'), float('nan'))
-        self.assertAlmostEqual(self.cmp.dist('Coiln', 'Colin'), float('nan'))
+        self.assertAlmostEqual(self.cmp.dist('Nigel', 'Niall'), 0.3776670227)
+        self.assertAlmostEqual(self.cmp.dist('Niall', 'Nigel'), 0.3776670227)
+        self.assertAlmostEqual(self.cmp.dist('Colin', 'Coiln'), 0.2928932188)
+        self.assertAlmostEqual(self.cmp.dist('Coiln', 'Colin'), 0.2928932188)
         self.assertAlmostEqual(
             self.cmp.dist('ATCAACGAGT', 'AACGATTAG'), 0.4880401968
         )
