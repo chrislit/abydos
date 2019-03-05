6d2eb9a415804739e22730b153835a279d7df602
Chris Little <chrisclittle@gmail.com>
2019-03-05 12:03:26 -0800

    base case, sim/corr

--- abydos/distance/_dispersion.py
+++ abydos/distance/_dispersion.py
@@ -18,7 +18,7 @@
 
 """abydos.distance._dispersion.
 
-Dispersion similarity
+Dispersion correlation
 """
 
 from __future__ import (
@@ -34,14 +34,14 @@ __all__ = ['Dispersion']
 
 
 class Dispersion(_TokenDistance):
-    r"""Dispersion similarity.
+    r"""Dispersion correlation.
 
     For two sets X and Y and a population N, the dispersion
-    similarity :cite:`IBM:2017` is
+    correlation :cite:`IBM:2017` is
 
         .. math::
 
-            sim_{dispersion}(X, Y) =
+            corr_{dispersion}(X, Y) =
             \frac{|X \cap Y| \cdot |(N \setminus X) \setminus Y| -
             |X \setminus Y| \cdot |Y \setminus X|}
             {|N|^2}
@@ -51,7 +51,7 @@ class Dispersion(_TokenDistance):
 
         .. math::
 
-            sim_{dispersion} =
+            corr_{dispersion} =
             \frac{ad-bc}{n^2}
 
     .. versionadded:: 0.4.0
@@ -105,6 +105,50 @@ class Dispersion(_TokenDistance):
             **kwargs
         )
 
+    def corr(self, src, tar):
+        """Return the Dispersion correlation of two strings.
+
+        Parameters
+        ----------
+        src : str
+            Source string (or QGrams/Counter objects) for comparison
+        tar : str
+            Target string (or QGrams/Counter objects) for comparison
+
+        Returns
+        -------
+        float
+            Dispersion correlation
+
+        Examples
+        --------
+        >>> cmp = Dispersion()
+        >>> cmp.corr('cat', 'hat')
+        0.0
+        >>> cmp.corr('Niall', 'Neil')
+        0.0
+        >>> cmp.corr('aluminum', 'Catalan')
+        0.0
+        >>> cmp.corr('ATCG', 'TAGC')
+        0.0
+
+
+        .. versionadded:: 0.4.0
+
+        """
+        self._tokenize(src, tar)
+
+        a = self._intersection_card()
+        b = self._src_only_card()
+        c = self._tar_only_card()
+        d = self._total_complement_card()
+        n = self._population_unique_card()
+
+        admbc = a * d - b * c
+        if admbc == 0.0:
+            return 0.0
+        return admbc / n ** 2
+
     def sim(self, src, tar):
         """Return the Dispersion similarity of two strings.
 
@@ -136,15 +180,7 @@ class Dispersion(_TokenDistance):
         .. versionadded:: 0.4.0
 
         """
-        self._tokenize(src, tar)
-
-        a = self._intersection_card()
-        b = self._src_only_card()
-        c = self._tar_only_card()
-        d = self._total_complement_card()
-        n = self._population_unique_card()
-
-        return (a * d - b * c) / n ** 2
+        return (1 + self.corr(src, tar)) / 2
 
 
 if __name__ == '__main__':
