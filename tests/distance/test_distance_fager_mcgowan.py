b676b297e8d46c156b525a2f2d3dbfc216e48213
Chris Little <chrisclittle@gmail.com>
2019-03-11 20:42:31 -0700

    corrected formula & added normalization by trimming to >=0.0

--- tests/distance/test_distance_fager_mcgowan.py
+++ tests/distance/test_distance_fager_mcgowan.py
@@ -44,39 +44,68 @@ class FagerMcGowanTestCases(unittest.TestCase):
     def test_fager_mcgowan_sim(self):
         """Test abydos.distance.FagerMcGowan.sim."""
         # Base cases
-        self.assertEqual(self.cmp.sim('', ''), float('nan'))
-        self.assertEqual(self.cmp.sim('a', ''), float('nan'))
-        self.assertEqual(self.cmp.sim('', 'a'), float('nan'))
-        self.assertEqual(self.cmp.sim('abc', ''), float('nan'))
-        self.assertEqual(self.cmp.sim('', 'abc'), float('nan'))
-        self.assertEqual(self.cmp.sim('abc', 'abc'), 0.0)
-        self.assertEqual(self.cmp.sim('abcd', 'efgh'), -1.118033988749895)
+        self.assertEqual(self.cmp.sim('', ''), 0.0)
+        self.assertEqual(self.cmp.sim('a', ''), 0.0)
+        self.assertEqual(self.cmp.sim('', 'a'), 0.0)
+        self.assertEqual(self.cmp.sim('abc', ''), 0.0)
+        self.assertEqual(self.cmp.sim('', 'abc'), 0.0)
+        self.assertEqual(self.cmp.sim('abc', 'abc'), 0.75)
+        self.assertEqual(self.cmp.sim('abcd', 'efgh'), 0.0)
 
-        self.assertAlmostEqual(self.cmp.sim('Nigel', 'Niall'), -0.7247448714)
-        self.assertAlmostEqual(self.cmp.sim('Niall', 'Nigel'), -0.7247448714)
-        self.assertAlmostEqual(self.cmp.sim('Colin', 'Coiln'), -0.7247448714)
-        self.assertAlmostEqual(self.cmp.sim('Coiln', 'Colin'), -0.7247448714)
+        self.assertAlmostEqual(self.cmp.sim('Nigel', 'Niall'), 0.2958758548)
+        self.assertAlmostEqual(self.cmp.sim('Niall', 'Nigel'), 0.2958758548)
+        self.assertAlmostEqual(self.cmp.sim('Colin', 'Coiln'), 0.2958758548)
+        self.assertAlmostEqual(self.cmp.sim('Coiln', 'Colin'), 0.2958758548)
         self.assertAlmostEqual(
-            self.cmp.sim('ATCAACGAGT', 'AACGATTAG'), -0.9908885827
+            self.cmp.sim('ATCAACGAGT', 'AACGATTAG'), 0.5166681402
         )
 
     def test_fager_mcgowan_dist(self):
         """Test abydos.distance.FagerMcGowan.dist."""
         # Base cases
-        self.assertEqual(self.cmp.dist('', ''), float('nan'))
-        self.assertEqual(self.cmp.dist('a', ''), float('nan'))
-        self.assertEqual(self.cmp.dist('', 'a'), float('nan'))
-        self.assertEqual(self.cmp.dist('abc', ''), float('nan'))
-        self.assertEqual(self.cmp.dist('', 'abc'), float('nan'))
-        self.assertEqual(self.cmp.dist('abc', 'abc'), 1.0)
-        self.assertEqual(self.cmp.dist('abcd', 'efgh'), 2.118033988749895)
+        self.assertEqual(self.cmp.dist('', ''), 1.0)
+        self.assertEqual(self.cmp.dist('a', ''), 1.0)
+        self.assertEqual(self.cmp.dist('', 'a'), 1.0)
+        self.assertEqual(self.cmp.dist('abc', ''), 1.0)
+        self.assertEqual(self.cmp.dist('', 'abc'), 1.0)
+        self.assertEqual(self.cmp.dist('abc', 'abc'), 0.25)
+        self.assertEqual(self.cmp.dist('abcd', 'efgh'), 1.0)
 
-        self.assertAlmostEqual(self.cmp.dist('Nigel', 'Niall'), 1.7247448714)
-        self.assertAlmostEqual(self.cmp.dist('Niall', 'Nigel'), 1.7247448714)
-        self.assertAlmostEqual(self.cmp.dist('Colin', 'Coiln'), 1.7247448714)
-        self.assertAlmostEqual(self.cmp.dist('Coiln', 'Colin'), 1.7247448714)
+        self.assertAlmostEqual(self.cmp.dist('Nigel', 'Niall'), 0.7041241452)
+        self.assertAlmostEqual(self.cmp.dist('Niall', 'Nigel'), 0.7041241452)
+        self.assertAlmostEqual(self.cmp.dist('Colin', 'Coiln'), 0.7041241452)
+        self.assertAlmostEqual(self.cmp.dist('Coiln', 'Colin'), 0.7041241452)
         self.assertAlmostEqual(
-            self.cmp.dist('ATCAACGAGT', 'AACGATTAG'), 1.9908885827
+            self.cmp.dist('ATCAACGAGT', 'AACGATTAG'), 0.4833318598
+        )
+
+    def test_fager_mcgowan_sim_score(self):
+        """Test abydos.distance.FagerMcGowan.sim_score."""
+        # Base cases
+        self.assertEqual(self.cmp.sim_score('', ''), 0.0)
+        self.assertEqual(self.cmp.sim_score('a', ''), 0.0)
+        self.assertEqual(self.cmp.sim_score('', 'a'), 0.0)
+        self.assertEqual(self.cmp.sim_score('abc', ''), 0.0)
+        self.assertEqual(self.cmp.sim_score('', 'abc'), 0.0)
+        self.assertEqual(self.cmp.sim_score('abc', 'abc'), 0.75)
+        self.assertEqual(
+            self.cmp.sim_score('abcd', 'efgh'), -0.22360679774997896
+        )
+
+        self.assertAlmostEqual(
+            self.cmp.sim_score('Nigel', 'Niall'), 0.2958758548
+        )
+        self.assertAlmostEqual(
+            self.cmp.sim_score('Niall', 'Nigel'), 0.2958758548
+        )
+        self.assertAlmostEqual(
+            self.cmp.sim_score('Colin', 'Coiln'), 0.2958758548
+        )
+        self.assertAlmostEqual(
+            self.cmp.sim_score('Coiln', 'Colin'), 0.2958758548
+        )
+        self.assertAlmostEqual(
+            self.cmp.sim_score('ATCAACGAGT', 'AACGATTAG'), 0.5166681402
         )
 
 
