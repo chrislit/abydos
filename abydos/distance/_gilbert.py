afc16b6022ddc8f06ac47ec78e66724ea68b131e
Chris Little <chrisclittle@gmail.com>
2019-03-18 12:35:50 -0700

    changed to corr

--- abydos/distance/_gilbert.py
+++ abydos/distance/_gilbert.py
@@ -18,7 +18,7 @@
 
 """abydos.distance._gilbert.
 
-Gilbert similarity
+Gilbert correlation
 """
 
 from __future__ import (
@@ -34,16 +34,16 @@ __all__ = ['Gilbert']
 
 
 class Gilbert(_TokenDistance):
-    r"""Gilbert similarity.
+    r"""Gilbert correlation.
 
-    For two sets X and Y and a population N, the Gilbert
-    similarity :cite:`Gilbert:1884` is
+    For two sets X and Y and a population N, the Gilbert correlation
+    :cite:`Gilbert:1884` is
 
         .. math::
 
-            sim_{Gilbert}(X, Y) =
+            corr_{Gilbert}(X, Y) =
             \frac{2(|X \cap Y| \cdot |(N \setminus X) \setminus Y| -
-            |X \setminus Y| \cdot |Y \setminus X|}
+            |X \setminus Y| \cdot |Y \setminus X|)}
             {|N|^2 - |X \cap Y|^2 + |X \setminus Y|^2 + |Y \setminus X|^2 -
             |(N \setminus X) \setminus Y|^2}
 
@@ -55,7 +55,7 @@ class Gilbert(_TokenDistance):
 
         .. math::
 
-            sim_{Gilbert} =
+            corr_{Gilbert} =
             \frac{2(ad-cd)}{n^2-a^2+b^2+c^2-d^2}
 
     .. versionadded:: 0.4.0
@@ -109,6 +109,52 @@ class Gilbert(_TokenDistance):
             **kwargs
         )
 
+    def corr(self, src, tar):
+        """Return the Gilbert correlation of two strings.
+
+        Parameters
+        ----------
+        src : str
+            Source string (or QGrams/Counter objects) for comparison
+        tar : str
+            Target string (or QGrams/Counter objects) for comparison
+
+        Returns
+        -------
+        float
+            Gilbert correlation
+
+        Examples
+        --------
+        >>> cmp = Gilbert()
+        >>> cmp.corr('cat', 'hat')
+        0.0
+        >>> cmp.corr('Niall', 'Neil')
+        0.0
+        >>> cmp.corr('aluminum', 'Catalan')
+        0.0
+        >>> cmp.corr('ATCG', 'TAGC')
+        0.0
+
+
+        .. versionadded:: 0.4.0
+
+        """
+        if src == tar:
+            return 1.0
+
+        self._tokenize(src, tar)
+
+        a = self._intersection_card()
+        b = self._src_only_card()
+        c = self._tar_only_card()
+        n = self._population_unique_card()
+
+        num = a * n - (a + b) * (a + c)
+        if num:
+            return num / (n * (a + b + c) - (a + b) * (a + c))
+        return 0.0
+
     def sim(self, src, tar):
         """Return the Gilbert similarity of two strings.
 
@@ -140,17 +186,7 @@ class Gilbert(_TokenDistance):
         .. versionadded:: 0.4.0
 
         """
-        self._tokenize(src, tar)
-
-        a = self._intersection_card()
-        b = self._src_only_card()
-        c = self._tar_only_card()
-        d = self._total_complement_card()
-        n = self._population_unique_card()
-
-        return (
-            2 * (a * d - b * c) / (n ** 2 - a ** 2 + b ** 2 + c ** 2 - d ** 2)
-        )
+        return (1.0 + self.corr(src, tar)) / 2.0
 
 
 if __name__ == '__main__':
