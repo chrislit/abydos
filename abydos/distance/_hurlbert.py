e91e3bf5ac07fb569f5b962df629c6fe95e08bf3
Chris Little <chrisclittle@gmail.com>
2019-03-23 15:39:26 -0700

    converted to corr, corner cases

--- abydos/distance/_hurlbert.py
+++ abydos/distance/_hurlbert.py
@@ -18,7 +18,7 @@
 
 """abydos.distance._hurlbert.
 
-Hurlbert similarity
+Hurlbert correlation
 """
 
 from __future__ import (
@@ -36,7 +36,7 @@ __all__ = ['Hurlbert']
 
 
 class Hurlbert(_TokenDistance):
-    r"""Hurlbert similarity.
+    r"""Hurlbert correlation.
 
     In :ref:`2x2 confusion table terms <confusion_table>`, where a+b+c+d=n,
     Hurlbert's coefficient of interspecific association :cite:`Hurlbert:1969`
@@ -44,7 +44,7 @@ class Hurlbert(_TokenDistance):
 
         .. math::
 
-            sim_{Hurlbert} =
+            corr_{Hurlbert} =
             \frac{ad-bc}{|ad-bc|} \sqrt{\frac{Obs_{\chi^2}-Min_{\chi^2}}
             {Max_{\chi^2}-Min_{\chi^2}}}
 
@@ -52,25 +52,28 @@ class Hurlbert(_TokenDistance):
 
         .. math::
 
-            Obs_{\chi^2} = \frac{(ad-bc)^2n}{(a+b)(a+c)(b+d)(c+d)}
+            \begin{array}{lll}
+            Obs_{\chi^2} &= \frac{(ad-bc)^2n}{(a+b)(a+c)(b+d)(c+d)}
 
-            Max_{\chi^2} = \frac{(a+b)(b+d)n}{(a+c)(c+d)} \textrm{ when }
+            Max_{\chi^2} &= \frac{(a+b)(b+d)n}{(a+c)(c+d)} &\textrm{ when }
             ad \geq bc
 
-            Max_{\chi^2} = \frac{(a+b)(a+c)n}{(b+d)(c+d)} \textrm{ when }
+            Max_{\chi^2} &= \frac{(a+b)(a+c)n}{(b+d)(c+d)} &\textrm{ when }
             ad < bc \textrm{ and } a \leq d
 
-            Max_{\chi^2} = \frac{(b+d)(c+d)n}{(a+b)(a+c)} \textrm{ when }
+            Max_{\chi^2} &= \frac{(b+d)(c+d)n}{(a+b)(a+c)} &\textrm{ when }
             ad < bc \textrm{ and } a > d
 
-            Min_{\chi^2} = \frac{n^3 (\hat{a} - g(\hat{a}))^2}
+            Min_{\chi^2} &= \frac{n^3 (\hat{a} - g(\hat{a}))^2}
             {(a+b)(a+c)(c+d)(b+d)}
 
-            \textrm{where } \hat{a} = \frac{(a+b)(a+c)}{n}
+            \textrm{where } \hat{a} &= \frac{(a+b)(a+c)}{n}
 
-            \textrm{and } g(\hat{a}) = \lfloor\hat{a}\rfloor
-            \textrm{ when } ad < bc
-            \textrm{, otherwise } g(\hat{a}) = \lceil\hat{a}\rceil
+            \textrm{and } g(\hat{a}) &= \lfloor\hat{a}\rfloor
+            &\textrm{ when } ad < bc,
+
+            \textrm{otherwise } g(\hat{a}) &= \lceil\hat{a}\rceil
+            \end{array}
 
     .. versionadded:: 0.4.0
     """
@@ -123,6 +126,71 @@ class Hurlbert(_TokenDistance):
             **kwargs
         )
 
+    def corr(self, src, tar):
+        """Return the Hurlbert correlation of two strings.
+
+        Parameters
+        ----------
+        src : str
+            Source string (or QGrams/Counter objects) for comparison
+        tar : str
+            Target string (or QGrams/Counter objects) for comparison
+
+        Returns
+        -------
+        float
+            Hurlbert correlation
+
+        Examples
+        --------
+        >>> cmp = Hurlbert()
+        >>> cmp.corr('cat', 'hat')
+        0.0
+        >>> cmp.corr('Niall', 'Neil')
+        0.0
+        >>> cmp.corr('aluminum', 'Catalan')
+        0.0
+        >>> cmp.corr('ATCG', 'TAGC')
+        0.0
+
+
+        .. versionadded:: 0.4.0
+
+        """
+        if src == tar:
+            return 1.0
+        if not src or not tar:
+            return -1.0
+
+        self._tokenize(src, tar)
+
+        a = self._intersection_card()
+        b = self._src_only_card()
+        c = self._tar_only_card()
+        d = self._total_complement_card()
+        n = self._population_unique_card()
+
+        admbc = a * d - b * c
+        marginals_product = (a + b) * (a + c) * (b + d) * (c + d)
+
+        obs_chisq = admbc * admbc * n / marginals_product
+
+        if a * d >= b * c:
+            max_chisq = (a + b) * (b + d) * n / ((a + c) * (c + d))
+        elif a <= d:
+            max_chisq = (a + b) * (a + c) * n / ((b + d) * (c + d))
+        else:
+            max_chisq = (b + d) * (c + d) * n / ((a + b) * (a + c))
+
+        a_hat = (a + b) * (a + c) / n
+        g_a_hat = ceil(a_hat) if a * d < b * c else floor(a_hat)
+
+        min_chisq = n ** 3 * (a_hat - g_a_hat) ** 2 / marginals_product
+
+        return copysign(
+            ((obs_chisq - min_chisq) / (max_chisq - min_chisq)) ** 0.5, admbc
+        )
+
     def sim(self, src, tar):
         """Return the Hurlbert similarity of two strings.
 
@@ -154,33 +222,7 @@ class Hurlbert(_TokenDistance):
         .. versionadded:: 0.4.0
 
         """
-        self._tokenize(src, tar)
-
-        a = self._intersection_card()
-        b = self._src_only_card()
-        c = self._tar_only_card()
-        d = self._total_complement_card()
-        n = self._population_unique_card()
-
-        admbc = a * d - b * c
-        marginals_product = (a + b) * (a + c) * (b + d) * (c + d)
-
-        obs_chisq = admbc ** 2 * n / marginals_product
-        if a * d >= b * c:
-            max_chisq = (a + b) * (b + d) * n / ((a + c) * (c + d))
-        elif a <= d:
-            max_chisq = (a + b) * (a + c) * n / ((b + d) * (c + d))
-        else:
-            max_chisq = (b + d) * (c + d) * n / ((a + b) * (a + c))
-
-        a_hat = (a + b) * (a + c) / n
-        g_a_hat = ceil(a_hat) if a * d < b * c else floor(a_hat)
-
-        min_chisq = n ** 2 * (a_hat - g_a_hat) ** 2 / marginals_product
-
-        return copysign(
-            ((obs_chisq - min_chisq) / (max_chisq - min_chisq)) ** 0.5, admbc
-        )
+        return (1.0 + self.corr(src, tar)) / 2.0
 
 
 if __name__ == '__main__':
