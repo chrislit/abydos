c5c019c7aec0c404212ef365c9ddf0d755e894e0
Chris Little <chrisclittle@gmail.com>
2019-03-27 13:00:15 -0700

    sim to corr, corner cases, test updates

--- abydos/distance/_maxwell_pilliner.py
+++ abydos/distance/_maxwell_pilliner.py
@@ -18,7 +18,7 @@
 
 """abydos.distance._maxwell_pilliner.
 
-Maxwell & Pilliner similarity
+Maxwell & Pilliner correlation
 """
 
 from __future__ import (
@@ -34,14 +34,14 @@ __all__ = ['MaxwellPilliner']
 
 
 class MaxwellPilliner(_TokenDistance):
-    r"""Maxwell & Pilliner similarity.
+    r"""Maxwell & Pilliner correlation.
 
-    For two sets X and Y and a population N, Maxwell & Pilliner similarity
+    For two sets X and Y and a population N, Maxwell & Pilliner correlation
     :cite:`Maxwell:1968` is
 
         .. math::
 
-            sim_{MaxwellPilliner}(X, Y) =
+            corr_{MaxwellPilliner}(X, Y) =
             \frac{2(|X \cap Y| \cdot |(N \setminus X) \setminus Y| -
             |X \setminus Y| \cdot |Y \setminus X|)}
             {|X| \cdot |N \setminus X| + |Y| \cdot |N \setminus Y|}
@@ -51,7 +51,7 @@ class MaxwellPilliner(_TokenDistance):
 
         .. math::
 
-            sim_{MaxwellPilliner} =
+            corr_{MaxwellPilliner} =
             \frac{2(ad-bc)}{(a+b)(c+d)+(a+c)(b+c)}
 
     .. versionadded:: 0.4.0
@@ -105,6 +105,49 @@ class MaxwellPilliner(_TokenDistance):
             **kwargs
         )
 
+    def corr(self, src, tar):
+        """Return the Maxwell & Pilliner correlation of two strings.
+
+        Parameters
+        ----------
+        src : str
+            Source string (or QGrams/Counter objects) for comparison
+        tar : str
+            Target string (or QGrams/Counter objects) for comparison
+
+        Returns
+        -------
+        float
+            Maxwell & Pilliner correlation
+
+        Examples
+        --------
+        >>> cmp = MaxwellPilliner()
+        >>> cmp.corr('cat', 'hat')
+        0.0
+        >>> cmp.corr('Niall', 'Neil')
+        0.0
+        >>> cmp.corr('aluminum', 'Catalan')
+        0.0
+        >>> cmp.corr('ATCG', 'TAGC')
+        0.0
+
+
+        .. versionadded:: 0.4.0
+
+        """
+        self._tokenize(src, tar)
+
+        a = self._intersection_card()
+        b = self._src_only_card()
+        c = self._tar_only_card()
+        d = self._total_complement_card()
+
+        num = a * d - b * c
+        if num:
+            return 2 * num / ((a + b) * (c + d) + (a + c) * (b + d))
+        return 0.0
+
     def sim(self, src, tar):
         """Return the Maxwell & Pilliner similarity of two strings.
 
@@ -136,14 +179,7 @@ class MaxwellPilliner(_TokenDistance):
         .. versionadded:: 0.4.0
 
         """
-        self._tokenize(src, tar)
-
-        a = self._intersection_card()
-        b = self._src_only_card()
-        c = self._tar_only_card()
-        d = self._total_complement_card()
-
-        return 2 * (a * d - b * c) / ((a + b) * (c + d) + (a + c) * (b + d))
+        return (1.0 + self.corr(src, tar)) / 2.0
 
 
 if __name__ == '__main__':
