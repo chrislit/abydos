e2702141e04192f86175105569ce2a92a6865488
Chris Little <chrisclittle@gmail.com>
2019-03-27 13:56:58 -0700

    sim to corr, corner cases, updated tests

--- abydos/distance/_mcewen_michael.py
+++ abydos/distance/_mcewen_michael.py
@@ -18,7 +18,7 @@
 
 """abydos.distance._mcewen_michael.
 
-McEwen & Michael similarity
+McEwen & Michael correlation
 """
 
 from __future__ import (
@@ -34,14 +34,14 @@ __all__ = ['McEwenMichael']
 
 
 class McEwenMichael(_TokenDistance):
-    r"""McEwen & Michael similarity.
+    r"""McEwen & Michael correlation.
 
     For two sets X and Y and a population N, the McEwen & Michael
-    similarity :cite:`Michael:1920` is
+    correlation :cite:`Michael:1920` is
 
         .. math::
 
-            sim_{McEwenMichael}(X, Y) =
+            corr_{McEwenMichael}(X, Y) =
             \frac{4(|X \cap Y| \cdot |(N \setminus X) \setminus Y| -
             |X \setminus Y| \cdot |Y \setminus X|)}
             {(|X \cap Y| + |(N \setminus X) \setminus Y|)^2 +
@@ -52,7 +52,7 @@ class McEwenMichael(_TokenDistance):
 
         .. math::
 
-            sim_{McEwenMichael} =
+            corr_{McEwenMichael} =
             \frac{4(ad-bc)}{(a+d)^2+(b+c)^2}
 
     .. versionadded:: 0.4.0
@@ -106,6 +106,50 @@ class McEwenMichael(_TokenDistance):
             **kwargs
         )
 
+    def corr(self, src, tar):
+        """Return the McEwen & Michael correlation of two strings.
+
+        Parameters
+        ----------
+        src : str
+            Source string (or QGrams/Counter objects) for comparison
+        tar : str
+            Target string (or QGrams/Counter objects) for comparison
+
+        Returns
+        -------
+        float
+            Michael correlation
+
+        Examples
+        --------
+        >>> cmp = McEwenMichael()
+        >>> cmp.corr('cat', 'hat')
+        0.0
+        >>> cmp.corr('Niall', 'Neil')
+        0.0
+        >>> cmp.corr('aluminum', 'Catalan')
+        0.0
+        >>> cmp.corr('ATCG', 'TAGC')
+        0.0
+
+
+        .. versionadded:: 0.4.0
+
+        """
+        self._tokenize(src, tar)
+
+        a = self._intersection_card()
+        b = self._src_only_card()
+        c = self._tar_only_card()
+        d = self._total_complement_card()
+
+        num = a * d - b * c
+
+        if num:
+            return 4 * num / ((a + d) ** 2 + (b + c) ** 2)
+        return 0.0
+
     def sim(self, src, tar):
         """Return the McEwen & Michael similarity of two strings.
 
@@ -137,14 +181,7 @@ class McEwenMichael(_TokenDistance):
         .. versionadded:: 0.4.0
 
         """
-        self._tokenize(src, tar)
-
-        a = self._intersection_card()
-        b = self._src_only_card()
-        c = self._tar_only_card()
-        d = self._total_complement_card()
-
-        return 4 * (a * d - b * c) / ((a + d) ** 2 + (b + c) ** 2)
+        return (1.0 + self.corr(src, tar)) / 2.0
 
 
 if __name__ == '__main__':
