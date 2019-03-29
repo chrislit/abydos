0e925be4c185a6615b6c78d13b7f02f403715f64
Chris Little <chrisclittle@gmail.com>
2019-03-28 21:05:08 -0700

    added sim, corner cases

--- tests/distance/test_distance_peirce.py
+++ tests/distance/test_distance_peirce.py
@@ -42,14 +42,86 @@ class PeirceTestCases(unittest.TestCase):
     cmp = Peirce()
     cmp_no_d = Peirce(alphabet=0)
 
+    def test_peirce_sim(self):
+        """Test abydos.distance.Peirce.sim."""
+        # Base cases
+        self.assertEqual(self.cmp.sim('', ''), 1.0)
+        self.assertEqual(self.cmp.sim('a', ''), 0.5)
+        self.assertEqual(self.cmp.sim('', 'a'), 0.5)
+        self.assertEqual(self.cmp.sim('abc', ''), 0.5)
+        self.assertEqual(self.cmp.sim('', 'abc'), 0.5)
+        self.assertEqual(self.cmp.sim('abc', 'abc'), 1.0)
+        self.assertEqual(self.cmp.sim('abcd', 'efgh'), 0.496790757381258)
+
+        self.assertAlmostEqual(self.cmp.sim('Nigel', 'Niall'), 0.7480719794)
+        self.assertAlmostEqual(self.cmp.sim('Niall', 'Nigel'), 0.7480719794)
+        self.assertAlmostEqual(self.cmp.sim('Colin', 'Coiln'), 0.7480719794)
+        self.assertAlmostEqual(self.cmp.sim('Coiln', 'Colin'), 0.7480719794)
+        self.assertAlmostEqual(
+            self.cmp.sim('ATCAACGAGT', 'AACGATTAG'), 0.8162413266
+        )
+
+        # Tests with alphabet=0 (no d factor)
+        self.assertEqual(self.cmp_no_d.sim('', ''), 1.0)
+        self.assertEqual(self.cmp_no_d.sim('a', ''), 0.5)
+        self.assertEqual(self.cmp_no_d.sim('', 'a'), 0.5)
+        self.assertEqual(self.cmp_no_d.sim('abc', ''), 0.5)
+        self.assertEqual(self.cmp_no_d.sim('', 'abc'), 0.5)
+        self.assertEqual(self.cmp_no_d.sim('abc', 'abc'), 1.0)
+        self.assertEqual(self.cmp_no_d.sim('abcd', 'efgh'), 0.0)
+
+        self.assertAlmostEqual(self.cmp_no_d.sim('Nigel', 'Niall'), 0.25)
+        self.assertAlmostEqual(self.cmp_no_d.sim('Niall', 'Nigel'), 0.25)
+        self.assertAlmostEqual(self.cmp_no_d.sim('Colin', 'Coiln'), 0.25)
+        self.assertAlmostEqual(self.cmp_no_d.sim('Coiln', 'Colin'), 0.25)
+        self.assertAlmostEqual(
+            self.cmp_no_d.sim('ATCAACGAGT', 'AACGATTAG'), 0.3181818182
+        )
+
+    def test_peirce_dist(self):
+        """Test abydos.distance.Peirce.dist."""
+        # Base cases
+        self.assertEqual(self.cmp.dist('', ''), 0.0)
+        self.assertEqual(self.cmp.dist('a', ''), 0.5)
+        self.assertEqual(self.cmp.dist('', 'a'), 0.5)
+        self.assertEqual(self.cmp.dist('abc', ''), 0.5)
+        self.assertEqual(self.cmp.dist('', 'abc'), 0.5)
+        self.assertEqual(self.cmp.dist('abc', 'abc'), 0.0)
+        self.assertEqual(self.cmp.dist('abcd', 'efgh'), 0.503209242618742)
+
+        self.assertAlmostEqual(self.cmp.dist('Nigel', 'Niall'), 0.2519280206)
+        self.assertAlmostEqual(self.cmp.dist('Niall', 'Nigel'), 0.2519280206)
+        self.assertAlmostEqual(self.cmp.dist('Colin', 'Coiln'), 0.2519280206)
+        self.assertAlmostEqual(self.cmp.dist('Coiln', 'Colin'), 0.2519280206)
+        self.assertAlmostEqual(
+            self.cmp.dist('ATCAACGAGT', 'AACGATTAG'), 0.1837586734
+        )
+
+        # Tests with alphabet=0 (no d factor)
+        self.assertEqual(self.cmp_no_d.dist('', ''), 0.0)
+        self.assertEqual(self.cmp_no_d.dist('a', ''), 0.5)
+        self.assertEqual(self.cmp_no_d.dist('', 'a'), 0.5)
+        self.assertEqual(self.cmp_no_d.dist('abc', ''), 0.5)
+        self.assertEqual(self.cmp_no_d.dist('', 'abc'), 0.5)
+        self.assertEqual(self.cmp_no_d.dist('abc', 'abc'), 0.0)
+        self.assertEqual(self.cmp_no_d.dist('abcd', 'efgh'), 1.0)
+
+        self.assertAlmostEqual(self.cmp_no_d.dist('Nigel', 'Niall'), 0.75)
+        self.assertAlmostEqual(self.cmp_no_d.dist('Niall', 'Nigel'), 0.75)
+        self.assertAlmostEqual(self.cmp_no_d.dist('Colin', 'Coiln'), 0.75)
+        self.assertAlmostEqual(self.cmp_no_d.dist('Coiln', 'Colin'), 0.75)
+        self.assertAlmostEqual(
+            self.cmp_no_d.dist('ATCAACGAGT', 'AACGATTAG'), 0.6818181818
+        )
+
     def test_peirce_corr(self):
         """Test abydos.distance.Peirce.corr."""
         # Base cases
-        self.assertEqual(self.cmp.corr('', ''), float('nan'))
+        self.assertEqual(self.cmp.corr('', ''), 1.0)
         self.assertEqual(self.cmp.corr('a', ''), 0.0)
-        self.assertEqual(self.cmp.corr('', 'a'), float('nan'))
+        self.assertEqual(self.cmp.corr('', 'a'), 0.0)
         self.assertEqual(self.cmp.corr('abc', ''), 0.0)
-        self.assertEqual(self.cmp.corr('', 'abc'), float('nan'))
+        self.assertEqual(self.cmp.corr('', 'abc'), 0.0)
         self.assertEqual(self.cmp.corr('abc', 'abc'), 1.0)
         self.assertEqual(self.cmp.corr('abcd', 'efgh'), -0.006418485237483954)
 
@@ -62,12 +134,12 @@ class PeirceTestCases(unittest.TestCase):
         )
 
         # Tests with alphabet=0 (no d factor)
-        self.assertEqual(self.cmp_no_d.corr('', ''), float('nan'))
-        self.assertEqual(self.cmp_no_d.corr('a', ''), float('nan'))
-        self.assertEqual(self.cmp_no_d.corr('', 'a'), float('nan'))
-        self.assertEqual(self.cmp_no_d.corr('abc', ''), float('nan'))
-        self.assertEqual(self.cmp_no_d.corr('', 'abc'), float('nan'))
-        self.assertEqual(self.cmp_no_d.corr('abc', 'abc'), float('nan'))
+        self.assertEqual(self.cmp_no_d.corr('', ''), 1.0)
+        self.assertEqual(self.cmp_no_d.corr('a', ''), 0.0)
+        self.assertEqual(self.cmp_no_d.corr('', 'a'), 0.0)
+        self.assertEqual(self.cmp_no_d.corr('abc', ''), 0.0)
+        self.assertEqual(self.cmp_no_d.corr('', 'abc'), 0.0)
+        self.assertEqual(self.cmp_no_d.corr('abc', 'abc'), 1.0)
         self.assertEqual(self.cmp_no_d.corr('abcd', 'efgh'), -1.0)
 
         self.assertAlmostEqual(self.cmp_no_d.corr('Nigel', 'Niall'), -0.5)
