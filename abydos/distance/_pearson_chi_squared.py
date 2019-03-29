cd3667d71b1e5f0fd483921a6b328bff0e9e2106
Chris Little <chrisclittle@gmail.com>
2019-03-28 19:10:05 -0700

    removed unneeded extra base case

--- abydos/distance/_pearson_chi_squared.py
+++ abydos/distance/_pearson_chi_squared.py
@@ -155,10 +155,7 @@ class PearsonChiSquared(_TokenDistance):
         if not src or not tar:
             return 0.0
 
-        num = n * (a * d - b * c) ** 2
-        if num:
-            return num / (ab * ac * (b + d) * (c + d))
-        return 0.0
+        return n * (a * d - b * c) ** 2 / (ab * ac * (b + d) * (c + d))
 
     def corr(self, src, tar):
         """Return Pearson's Chi-Squared correlation of two strings.
