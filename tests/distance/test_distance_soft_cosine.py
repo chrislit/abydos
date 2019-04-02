4b310fad04f6e2900a1ddef6fd70e13154150670
Chris Little <chrisclittle@gmail.com>
2019-04-02 12:34:59 -0700

    added cases to complete coverage

--- tests/distance/test_distance_soft_cosine.py
+++ tests/distance/test_distance_soft_cosine.py
@@ -40,6 +40,9 @@ class SoftCosineTestCases(unittest.TestCase):
     """
 
     cmp = SoftCosine()
+    cmp_b = SoftCosine(sim_method='b')
+    cmp_c = SoftCosine(sim_method='c')
+    cmp_d = SoftCosine(sim_method='d')
 
     def test_soft_cosine_sim(self):
         """Test abydos.distance.SoftCosine.sim."""
@@ -60,6 +63,62 @@ class SoftCosineTestCases(unittest.TestCase):
             self.cmp.sim('ATCAACGAGT', 'AACGATTAG'), 0.9731507012
         )
 
+        # Constructor exception
+        with self.assertRaises(ValueError):
+            SoftCosine(sim_method='e')
+
+        # Alternate sim_methods
+        # Base cases
+        self.assertEqual(self.cmp_b.sim('', ''), 1.0)
+        self.assertEqual(self.cmp_b.sim('a', ''), 0.0)
+        self.assertEqual(self.cmp_b.sim('', 'a'), 0.0)
+        self.assertEqual(self.cmp_b.sim('abc', ''), 0.0)
+        self.assertEqual(self.cmp_b.sim('', 'abc'), 0.0)
+        self.assertEqual(self.cmp_b.sim('abc', 'abc'), 1.0)
+        self.assertAlmostEqual(self.cmp_b.sim('abcd', 'efgh'), 0.2)
+
+        self.assertAlmostEqual(self.cmp_b.sim('Nigel', 'Niall'), 0.721687836)
+        self.assertAlmostEqual(self.cmp_b.sim('Niall', 'Nigel'), 0.721687836)
+        self.assertAlmostEqual(self.cmp_b.sim('Colin', 'Coiln'), 1.0)
+        self.assertAlmostEqual(self.cmp_b.sim('Coiln', 'Colin'), 1.0)
+        self.assertAlmostEqual(
+            self.cmp_b.sim('ATCAACGAGT', 'AACGATTAG'), 0.98328200498
+        )
+
+        # Base cases
+        self.assertEqual(self.cmp_c.sim('', ''), 1.0)
+        self.assertEqual(self.cmp_c.sim('a', ''), 0.0)
+        self.assertEqual(self.cmp_c.sim('', 'a'), 0.0)
+        self.assertEqual(self.cmp_c.sim('abc', ''), 0.0)
+        self.assertEqual(self.cmp_c.sim('', 'abc'), 0.0)
+        self.assertEqual(self.cmp_c.sim('abc', 'abc'), 1.0)
+        self.assertAlmostEqual(self.cmp_c.sim('abcd', 'efgh'), 0.2828427124746)
+
+        self.assertAlmostEqual(self.cmp_c.sim('Nigel', 'Niall'), 0.800818463)
+        self.assertAlmostEqual(self.cmp_c.sim('Niall', 'Nigel'), 0.800818463)
+        self.assertAlmostEqual(self.cmp_c.sim('Colin', 'Coiln'), 1.207106781)
+        self.assertAlmostEqual(self.cmp_c.sim('Coiln', 'Colin'), 1.207106781)
+        self.assertAlmostEqual(
+            self.cmp_c.sim('ATCAACGAGT', 'AACGATTAG'), 1.023072064
+        )
+
+        # Base cases
+        self.assertEqual(self.cmp_d.sim('', ''), 1.0)
+        self.assertEqual(self.cmp_d.sim('a', ''), 0.0)
+        self.assertEqual(self.cmp_d.sim('', 'a'), 0.0)
+        self.assertEqual(self.cmp_d.sim('abc', ''), 0.0)
+        self.assertEqual(self.cmp_d.sim('', 'abc'), 0.0)
+        self.assertEqual(self.cmp_d.sim('abc', 'abc'), 1.0)
+        self.assertAlmostEqual(self.cmp_d.sim('abcd', 'efgh'), 0.1)
+
+        self.assertAlmostEqual(self.cmp_d.sim('Nigel', 'Niall'), 0.6172133998)
+        self.assertAlmostEqual(self.cmp_d.sim('Niall', 'Nigel'), 0.6172133998)
+        self.assertAlmostEqual(self.cmp_d.sim('Colin', 'Coiln'), 0.75)
+        self.assertAlmostEqual(self.cmp_d.sim('Coiln', 'Colin'), 0.75)
+        self.assertAlmostEqual(
+            self.cmp_d.sim('ATCAACGAGT', 'AACGATTAG'), 0.89597867038
+        )
+
     def test_soft_cosine_dist(self):
         """Test abydos.distance.SoftCosine.dist."""
         # Base cases
@@ -69,7 +128,7 @@ class SoftCosineTestCases(unittest.TestCase):
         self.assertEqual(self.cmp.dist('abc', ''), 1.0)
         self.assertEqual(self.cmp.dist('', 'abc'), 1.0)
         self.assertEqual(self.cmp.dist('abc', 'abc'), 0.0)
-        self.assertEqual(self.cmp.dist('abcd', 'efgh'), 0.25714285714285734)
+        self.assertAlmostEqual(self.cmp.dist('abcd', 'efgh'), 0.25714285714285734)
 
         self.assertAlmostEqual(self.cmp.dist('Nigel', 'Niall'), 0.101853761)
         self.assertAlmostEqual(self.cmp.dist('Niall', 'Nigel'), 0.101853761)
