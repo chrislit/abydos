35d13c81860b8f6299747edc91e96e403cfd63c9
Chris Little <chrisclittle@gmail.com>
2019-06-11 14:52:31 -0700

    switched from dist to sim

--- tests/distance/test_distance_isg.py
+++ tests/distance/test_distance_isg.py
@@ -41,41 +41,41 @@ class ISGTestCases(unittest.TestCase):
 
     cmp = ISG()
 
-    def test_inclusion_dist(self):
-        """Test abydos.distance.ISG.dist."""
+    def test_inclusion_sim(self):
+        """Test abydos.distance.ISG.sim."""
         # Base cases
-        self.assertEqual(self.cmp.dist('', ''), 0.0)
-        self.assertEqual(self.cmp.dist('a', ''), 1.0)
-        self.assertEqual(self.cmp.dist('', 'a'), 1.0)
-        self.assertEqual(self.cmp.dist('a', 'a'), 0.0)
-        self.assertEqual(self.cmp.dist('abc', ''), 1.0)
-        self.assertEqual(self.cmp.dist('', 'abc'), 1.0)
-        self.assertEqual(self.cmp.dist('abc', 'abc'), 0.0)
-        self.assertEqual(self.cmp.dist('abcd', 'efgh'), 1.0)
+        self.assertEqual(self.cmp.sim('', ''), 0.0)
+        self.assertEqual(self.cmp.sim('a', ''), 1.0)
+        self.assertEqual(self.cmp.sim('', 'a'), 1.0)
+        self.assertEqual(self.cmp.sim('a', 'a'), 0.0)
+        self.assertEqual(self.cmp.sim('abc', ''), 1.0)
+        self.assertEqual(self.cmp.sim('', 'abc'), 1.0)
+        self.assertEqual(self.cmp.sim('abc', 'abc'), 0.0)
+        self.assertEqual(self.cmp.sim('abcd', 'efgh'), 1.0)
 
         # Tescases from paper
-        self.assertEqual(self.cmp.dist('alaire', 'alard'), 0.5714285714285714)
+        self.assertEqual(self.cmp.sim('alaire', 'alard'), 0.5714285714285714)
         self.assertEqual(
-            self.cmp.dist('georges', 'george'), 0.8571428571428571
+            self.cmp.sim('georges', 'george'), 0.8571428571428571
         )
-        self.assertEqual(self.cmp.dist('emile', 'emilien'), 0.7142857142857143)
-        self.assertEqual(self.cmp.dist('blanchet', 'blanchette'), 0.8)
-        self.assertEqual(self.cmp.dist('marie', 'maria'), 0.6666666666666666)
-        self.assertEqual(self.cmp.dist('filion', 'filguion'), 0.75)
+        self.assertEqual(self.cmp.sim('emile', 'emilien'), 0.7142857142857143)
+        self.assertEqual(self.cmp.sim('blanchet', 'blanchette'), 0.8)
+        self.assertEqual(self.cmp.sim('marie', 'maria'), 0.6666666666666666)
+        self.assertEqual(self.cmp.sim('filion', 'filguion'), 0.75)
         self.assertEqual(
-            self.cmp.dist('daneau', 'dagneau'), 0.8571428571428571
+            self.cmp.sim('daneau', 'dagneau'), 0.8571428571428571
         )
-        self.assertEqual(self.cmp.dist('larouche', 'laroche'), 0.875)
+        self.assertEqual(self.cmp.sim('larouche', 'laroche'), 0.875)
         self.assertEqual(
-            self.cmp.dist('alaire', 'dalaire'), 0.8571428571428571
+            self.cmp.sim('alaire', 'dalaire'), 0.8571428571428571
         )
-        self.assertEqual(self.cmp.dist('donne', 'dionne'), 0.8333333333333334)
-        self.assertEqual(self.cmp.dist('audet', 'gaudet'), 0.8333333333333334)
-        self.assertEqual(self.cmp.dist('couet', 'caouet'), 0.8333333333333334)
-        self.assertEqual(self.cmp.dist('exulie', 'axilia'), 0.5)
-        self.assertEqual(self.cmp.dist('leon', 'noel'), 0.3333333333333333)
+        self.assertEqual(self.cmp.sim('donne', 'dionne'), 0.8333333333333334)
+        self.assertEqual(self.cmp.sim('audet', 'gaudet'), 0.8333333333333334)
+        self.assertEqual(self.cmp.sim('couet', 'caouet'), 0.8333333333333334)
+        self.assertEqual(self.cmp.sim('exulie', 'axilia'), 0.5)
+        self.assertEqual(self.cmp.sim('leon', 'noel'), 0.3333333333333333)
         self.assertEqual(
-            self.cmp.dist('norbert', 'bertran'), 0.16666666666666666
+            self.cmp.sim('norbert', 'bertran'), 0.16666666666666666
         )
 
 
