9acf885b81cea8d2ec515344d4c1cbafaa7a7f55
Chris Little <chrisclittle@gmail.com>
2019-06-28 17:46:14 -0700

    first pass at discounted Levenshtein distance
    (needs tuning, maybe tunable params)

---
+++ abydos/distance/_discounted_levenshtein.py
@@ -0,0 +1,384 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 by Christopher C. Little.
+# This file is part of Abydos.
+#
+# Abydos is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Abydos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Abydos. If not, see <http://www.gnu.org/licenses/>.
+
+"""abydos.distance._discounted_levenshtein.
+
+Discounted Levenshtein edit distance
+"""
+
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+from math import log
+
+from numpy import float as np_float
+from numpy import zeros as np_zeros
+
+from six.moves import range
+
+from ._distance import _Distance
+
+__all__ = ['DiscountedLevenshtein']
+
+
+class DiscountedLevenshtein(_Distance):
+    """Discounted Levenshtein distance.
+
+    This is a variant of Levenshtein distance for which edits later in a string
+    have discounted cost, on the theory that earlier edits are less likely
+    than later ones.
+
+    .. versionadded:: 0.4.1
+    """
+
+    def __init__(
+            self,
+            mode='lev',
+            normalizer=max,
+            discount_from=1,
+            **kwargs
+    ):
+        """Initialize DiscountedLevenshtein instance.
+
+        Parameters
+        ----------
+        mode : str
+            Specifies a mode for computing the discounted Levenshtein distance:
+
+                - ``lev`` (default) computes the ordinary Levenshtein distance,
+                  in which edits may include inserts, deletes, and
+                  substitutions
+                - ``osa`` computes the Optimal String Alignment distance, in
+                  which edits may include inserts, deletes, substitutions, and
+                  transpositions but substrings may only be edited once
+
+        normalizer : function
+            A function that takes an list and computes a normalization term
+            by which the edit distance is divided (max by default). Another
+            good option is the sum function.
+        discount_from : int or str
+            If an int is supplied, this is the first character whose edit cost
+            will be discounted. If the str ``vowels`` is supplied, discounting
+            will start with the first non-vowel after the first vowel.
+        discount_factor :
+        **kwargs
+            Arbitrary keyword arguments
+
+
+        .. versionadded:: 0.4.1
+
+        """
+        super(DiscountedLevenshtein, self).__init__(**kwargs)
+        self._mode = mode
+        self._normalizer = normalizer
+        self._discount_from = discount_from
+
+    @staticmethod
+    def _cost(pos, discount_from=1):
+        return 1 / (log(1 + (pos - discount_from) / 5) + 1)
+
+    def _alignment_matrix(self, src, tar):
+        """Return the Levenshtein alignment matrix.
+
+        Parameters
+        ----------
+        src : str
+            Source string for comparison
+        tar : str
+            Target string for comparison
+
+        Returns
+        -------
+        numpy.ndarray
+            The alignment matrix
+
+
+        .. versionadded:: 0.4.1
+
+        """
+        src_len = len(src)
+        tar_len = len(tar)
+
+        if self._discount_from == 'vowels':
+            vowels = set('aeiou')
+            src_voc = src.lower()
+            discount_from = 0
+            for i in range(len(src_voc)):
+                if src_voc[i] in vowels:
+                    discount_from = i
+                    break
+            for i in range(discount_from, len(src_voc)):
+                if src_voc[i] not in vowels:
+                    discount_from = i
+                    break
+        elif isinstance(self._discount_from, int):
+            discount_from = self._discount_from
+        else:
+            discount_from = 1
+
+        d_mat = np_zeros((src_len + 1, tar_len + 1), dtype=np_float)
+        for i in range(src_len + 1):
+            d_mat[i, 0] = i * self._cost(i, discount_from)
+        for j in range(tar_len + 1):
+            d_mat[0, j] = j * self._cost(j, discount_from)
+
+        for i in range(src_len):
+            for j in range(tar_len):
+                d_mat[i + 1, j + 1] = min(
+                    d_mat[i + 1, j]
+                    + self._cost(max(i, j), discount_from),  # ins
+                    d_mat[i, j + 1]
+                    + self._cost(max(i, j), discount_from),  # del
+                    d_mat[i, j]
+                    + (
+                        self._cost(max(i, j), discount_from)
+                        if src[i] != tar[j]
+                        else 0
+                    ),  # sub/==
+                )
+
+                if self._mode == 'osa':
+                    if (
+                            i + 1 > 1
+                            and j + 1 > 1
+                            and src[i] == tar[j - 1]
+                            and src[i - 1] == tar[j]
+                    ):
+                        # transposition
+                        d_mat[i + 1, j + 1] = min(
+                            d_mat[i + 1, j + 1],
+                            d_mat[i - 1, j - 1]
+                            + self._cost(max(i, j), discount_from),
+                        )
+
+        return d_mat
+
+    def alignment(self, src, tar):
+        """Return the Levenshtein alignment of two strings.
+
+        Parameters
+        ----------
+        src : str
+            Source string for comparison
+        tar : str
+            Target string for comparison
+
+        Returns
+        -------
+        tuple
+            A tuple containing the Levenshtein distance and the two strings,
+            aligned.
+
+        Examples
+        --------
+        >>> cmp = DiscountedLevenshtein()
+        >>> cmp.alignment('cat', 'hat')
+        (1.0, 'cat', 'hat')
+        >>> cmp.alignment('Niall', 'Neil')
+        (3.0, 'Niall', 'Neil-')
+        >>> cmp.alignment('aluminum', 'Catalan')
+        (7.0, '-aluminum', 'Catalan--')
+        >>> cmp.alignment('ATCG', 'TAGC')
+        (3.0, 'ATCG-', '-TAGC')
+
+        >>> cmp = DiscountedLevenshtein(mode='osa')
+        >>> cmp.alignment('ATCG', 'TAGC')
+        (2.0, 'ATCG', 'TAGC')
+        >>> cmp.alignment('ACTG', 'TAGC')
+        (4.0, 'ACTG', 'TAGC')
+
+
+        .. versionadded:: 0.4.1
+
+        """
+        d_mat = self._alignment_matrix(src, tar)
+
+        src_aligned = []
+        tar_aligned = []
+
+        src_pos = len(src)
+        tar_pos = len(tar)
+
+        distance = d_mat[src_pos, tar_pos]
+
+        while src_pos and tar_pos:
+            up = d_mat[src_pos, tar_pos - 1]
+            left = d_mat[src_pos - 1, tar_pos]
+            diag = d_mat[src_pos - 1, tar_pos - 1]
+
+            if diag <= min(up, left):
+                src_pos -= 1
+                tar_pos -= 1
+                src_aligned.append(src[src_pos])
+                tar_aligned.append(tar[tar_pos])
+            elif up <= left:
+                tar_pos -= 1
+                src_aligned.append('-')
+                tar_aligned.append(tar[tar_pos])
+            else:
+                src_pos -= 1
+                src_aligned.append(src[src_pos])
+                tar_aligned.append('-')
+        while tar_pos:
+            tar_pos -= 1
+            tar_aligned.append(tar[tar_pos])
+            src_aligned.append('-')
+        while src_pos:
+            src_pos -= 1
+            src_aligned.append(src[src_pos])
+            tar_aligned.append('-')
+
+        return distance, ''.join(src_aligned[::-1]), ''.join(tar_aligned[::-1])
+
+    def dist_abs(self, src, tar):
+        """Return the Levenshtein distance between two strings.
+
+        Parameters
+        ----------
+        src : str
+            Source string for comparison
+        tar : str
+            Target string for comparison
+
+        Returns
+        -------
+        int (may return a float if cost has float values)
+            The Levenshtein distance between src & tar
+
+        Examples
+        --------
+        >>> cmp = DiscountedLevenshtein()
+        >>> cmp.dist_abs('cat', 'hat')
+        1
+        >>> cmp.dist_abs('Niall', 'Neil')
+        3
+        >>> cmp.dist_abs('aluminum', 'Catalan')
+        7
+        >>> cmp.dist_abs('ATCG', 'TAGC')
+        3
+
+        >>> cmp = DiscountedLevenshtein(mode='osa')
+        >>> cmp.dist_abs('ATCG', 'TAGC')
+        2
+        >>> cmp.dist_abs('ACTG', 'TAGC')
+        4
+
+
+        .. versionadded:: 0.4.1
+
+        """
+        src_len = len(src)
+        tar_len = len(tar)
+
+        if src == tar:
+            return 0
+
+        if isinstance(self._discount_from, int):
+            discount_from = self._discount_from
+        else:
+            discount_from = 1
+
+        if not src:
+            return sum(
+                self._cost(pos, discount_from) for pos in range(tar_len)
+            )
+        if not tar:
+            return sum(
+                self._cost(pos, discount_from) for pos in range(src_len)
+            )
+
+        d_mat = self._alignment_matrix(src, tar)
+
+        if int(d_mat[src_len, tar_len]) == d_mat[src_len, tar_len]:
+            return int(d_mat[src_len, tar_len])
+        else:
+            return d_mat[src_len, tar_len]
+
+    def dist(self, src, tar):
+        """Return the normalized Levenshtein distance between two strings.
+
+        The Levenshtein distance is normalized by dividing the Levenshtein
+        distance (calculated by any of the three supported methods) by the
+        greater of the number of characters in src times the cost of a delete
+        and the number of characters in tar times the cost of an insert.
+        For the case in which all operations have :math:`cost = 1`, this is
+        equivalent to the greater of the length of the two strings src & tar.
+
+        Parameters
+        ----------
+        src : str
+            Source string for comparison
+        tar : str
+            Target string for comparison
+
+        Returns
+        -------
+        float
+            The normalized Levenshtein distance between src & tar
+
+        Examples
+        --------
+        >>> cmp = DiscountedLevenshtein()
+        >>> round(cmp.dist('cat', 'hat'), 12)
+        0.333333333333
+        >>> round(cmp.dist('Niall', 'Neil'), 12)
+        0.6
+        >>> cmp.dist('aluminum', 'Catalan')
+        0.875
+        >>> cmp.dist('ATCG', 'TAGC')
+        0.75
+
+
+        .. versionadded:: 0.4.1
+
+        """
+        if src == tar:
+            return 0
+
+        if isinstance(self._discount_from, int):
+            discount_from = self._discount_from
+        else:
+            discount_from = 1
+
+        src_len = len(src)
+        tar_len = len(tar)
+
+        normalize_term = self._normalizer(
+            [
+                sum(
+                    self._cost(pos, discount_from)
+                    for pos in range(src_len)
+                ),
+                sum(
+                    self._cost(pos, discount_from)
+                    for pos in range(tar_len)
+                ),
+            ]
+        )
+
+        return self.dist_abs(src, tar) / normalize_term
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
