d30f0f93a7cd81f54c5030b5f6f2b69e3abaceaf
Chris Little <chrisclittle@gmail.com>
2019-07-11 21:57:53 -0700

    attempt to make key ordering consistent across versions

--- abydos/distance/_token_distance.py
+++ abydos/distance/_token_distance.py
@@ -575,7 +575,12 @@ class _TokenDistance(_Distance):
                 if sim >= self.params['threshold']:
                     pair[(src_tok, tar_tok)] = sim
 
-        for src_tok, tar_tok in sorted(pair, key=pair.get, reverse=True):
+        ordered_keys = [(pair[_], _[0], _[1]) for _ in pair]
+        ordered_keys.sort(key=lambda x: x[2])
+        ordered_keys.sort(key=lambda x: x[1])
+        ordered_keys.sort(key=lambda x: x[0], reverse=True)
+
+        for _, src_tok, tar_tok in ordered_keys:
             pairings = min(src_only[src_tok], tar_only[tar_tok])
             if pairings:
                 sim = pair[(src_tok, tar_tok)]
