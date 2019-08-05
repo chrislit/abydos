corrected alignment computation, tests for same

abydos/distance/_discounted_levenshtein.py
@@ -30,8 +30,7 @@ from __future__ import (
 
 from math import log
 
-from numpy import float as np_float
-from numpy import zeros as np_zeros
+import numpy as np
 
 from six.moves import range
 
@@ -182,25 +181,30 @@ class DiscountedLevenshtein(_Distance):
         else:
             discount_from = [1, 1]
 
-        d_mat = np_zeros((src_len + 1, tar_len + 1), dtype=np_float)
+        d_mat = np.zeros((src_len + 1, tar_len + 1), dtype=np.float)
+        trace_mat = np.zeros((src_len + 1, tar_len + 1), dtype=np.int8)
         for i in range(1, src_len + 1):
             d_mat[i, 0] = d_mat[i - 1, 0] + self._cost(
                 max(0, i - discount_from[0])
             )
+            trace_mat[i, 0] = 1
         for j in range(1, tar_len + 1):
             d_mat[0, j] = d_mat[0, j - 1] + self._cost(
                 max(0, j - discount_from[1])
             )
-
+            trace_mat[0, j] = 0
         for i in range(src_len):
             i_extend = self._cost(max(0, i - discount_from[0]))
             for j in range(tar_len):
+                traces = ((i + 1, j), (i, j + 1), (i, j))
                 cost = min(i_extend, self._cost(max(0, j - discount_from[1])))
-                d_mat[i + 1, j + 1] = min(
-                    d_mat[i + 1, j] + cost,  # ins
-                    d_mat[i, j + 1] + cost,  # del
-                    d_mat[i, j] + (cost if src[i] != tar[j] else 0),  # sub/==
+                opts = (
+                    d_mat[traces[0]] + cost,  # ins
+                    d_mat[traces[1]] + cost,  # del
+                    d_mat[traces[2]] + (cost if src[i] != tar[j] else 0),  # sub/==
                 )
+                d_mat[i + 1, j + 1] = min(opts)
+                trace_mat[i + 1, j + 1] = int(np.argmin(opts))
 
                 if self._mode == 'osa':
                     if (
@@ -213,8 +217,9 @@ class DiscountedLevenshtein(_Distance):
                         d_mat[i + 1, j + 1] = min(
                             d_mat[i + 1, j + 1], d_mat[i - 1, j - 1] + cost
                         )
+                        trace_mat[i + 1, j + 1] = 2
 
-        return d_mat
+        return d_mat, trace_mat
 
     def alignment(self, src, tar):
         """Return the Levenshtein alignment of two strings.
@@ -238,7 +243,7 @@ class DiscountedLevenshtein(_Distance):
         >>> cmp.alignment('cat', 'hat')
         (1.0, 'cat', 'hat')
         >>> cmp.alignment('Niall', 'Neil')
-        (2.526064024369237, 'N-iall', 'Neil--')
+        (2.526064024369237, 'N-iall', 'Nei-l-')
         >>> cmp.alignment('aluminum', 'Catalan')
         (5.053867269967515, '-aluminum', 'Catalan--')
         >>> cmp.alignment('ATCG', 'TAGC')
@@ -248,13 +253,13 @@ class DiscountedLevenshtein(_Distance):
         >>> cmp.alignment('ATCG', 'TAGC')
         (1.7482385137517997, 'ATCG', 'TAGC')
         >>> cmp.alignment('ACTG', 'TAGC')
-        (3.342270622531718, '-ACTG', 'TAGC-')
+        (3.342270622531718, '-A-CTG', 'TAGC--')
 
 
         .. versionadded:: 0.4.1
 
         """
-        d_mat = self._alignment_matrix(src, tar)
+        d_mat, trace_mat = self._alignment_matrix(src, tar)
 
         src_aligned = []
         tar_aligned = []
@@ -265,27 +270,26 @@ class DiscountedLevenshtein(_Distance):
         distance = d_mat[src_pos, tar_pos]
 
         while src_pos and tar_pos:
-            up = d_mat[src_pos, tar_pos - 1]
-            left = d_mat[src_pos - 1, tar_pos]
-            diag = d_mat[src_pos - 1, tar_pos - 1]
+            src_trace, tar_trace = (
+                (src_pos, tar_pos - 1),
+                (src_pos - 1, tar_pos),
+                (src_pos - 1, tar_pos - 1),
+            )[trace_mat[src_pos, tar_pos]]
 
-            if diag <= min(up, left):
-                src_pos -= 1
-                tar_pos -= 1
-                src_aligned.append(src[src_pos])
-                tar_aligned.append(tar[tar_pos])
-            elif up <= left:
-                tar_pos -= 1
-                src_aligned.append('-')
-                tar_aligned.append(tar[tar_pos])
-            else:
-                src_pos -= 1
-                src_aligned.append(src[src_pos])
+            if src_pos != src_trace and tar_pos != tar_trace:
+                src_aligned.append(src[src_trace])
+                tar_aligned.append(tar[tar_trace])
+            elif src_pos != src_trace:
+                src_aligned.append(src[src_trace])
                 tar_aligned.append('-')
+            else:
+                src_aligned.append('-')
+                tar_aligned.append(tar[tar_trace])
+            src_pos, tar_pos = src_trace, tar_trace
         while tar_pos:
             tar_pos -= 1
-            tar_aligned.append(tar[tar_pos])
             src_aligned.append('-')
+            tar_aligned.append(tar[tar_pos])
         while src_pos:
             src_pos -= 1
             src_aligned.append(src[src_pos])
@@ -305,7 +309,7 @@ class DiscountedLevenshtein(_Distance):
 
         Returns
         -------
-        int (may return a float if cost has float values)
+        float (may return a float if cost has float values)
             The Levenshtein distance between src & tar
 
         Examples
@@ -334,7 +338,7 @@ class DiscountedLevenshtein(_Distance):
         tar_len = len(tar)
 
         if src == tar:
-            return 0
+            return 0.0
 
         if isinstance(self._discount_from, int):
             discount_from = self._discount_from
@@ -352,7 +356,7 @@ class DiscountedLevenshtein(_Distance):
                 for pos in range(src_len)
             )
 
-        d_mat = self._alignment_matrix(src, tar)
+        d_mat, trace_mat = self._alignment_matrix(src, tar)
 
         if int(d_mat[src_len, tar_len]) == d_mat[src_len, tar_len]:
             return int(d_mat[src_len, tar_len])