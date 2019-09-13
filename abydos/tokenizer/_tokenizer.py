9dbdcb6ee559db921365c9fd937b817b3bdaa7ae
Chris Little <chrisclittle@gmail.com>
2019-09-13 14:52:09 -0700

    replaced log2(x) with log(x, 2)
    This is for Python2.7 compatability. It will be returned to log2
    when Python2.7 compat is removed, since it is about 2.5x faster.

--- abydos/tokenizer/_tokenizer.py
+++ abydos/tokenizer/_tokenizer.py
@@ -29,7 +29,7 @@ from __future__ import (
 )
 
 from collections import Counter
-from math import exp, log1p, log2
+from math import exp, log1p, log
 
 __all__ = ['_Tokenizer']
 
@@ -116,7 +116,7 @@ class _Tokenizer(object):
             counts = Counter(self._ordered_tokens)
             n = len(self._ordered_tokens)
             self._tokens = {
-                key: -(val / n) * log2(val / n) for key, val in counts.items()
+                key: -(val / n) * log(val / n, 2) for key, val in counts.items()
             }
             self._ordered_weights = [
                 self._tokens[tok] / counts[tok] for tok in self._ordered_tokens
