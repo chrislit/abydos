e69821560955b93b04f0d51fe2c38ddd5a88c7bd
Chris Little <chrisclittle@gmail.com>
2019-10-17 11:40:39 -0700

    added remaining formulae for CYs

--- abydos/distance/_cao.py
+++ abydos/distance/_cao.py
@@ -38,17 +38,74 @@ __all__ = ['Cao']
 class Cao(_TokenDistance):
     r"""Cao's CY dissimilarity.
 
-    Given :math:`X_{ij}` (the number of
-    individuals of speecies :math:`j` in sample :math:`i`), :math:`X_{kj}` (the
-    number of individuals of speecies :math:`j` in sample :math:`k`), and
-    :math:`N` (the total number of speecies present in both samples),
-    Cao dissimilarity (CYd) :cite:`Cao:1997` is:
+    Given :math:`X_{ij}` (the number of individuals of speecies :math:`j` in
+    sample :math:`i`), :math:`X_{kj}` (the number of individuals of speecies
+    :math:`j` in sample :math:`k`), and :math:`N` (the total number of speecies
+    present in both samples), Cao dissimilarity (CYd) :cite:`Cao:1997` is:
 
     .. math::
 
         CYd = \frac{1}{N}\sum\Bigg(\frac{(X_{ij} + X_{kj})log_{10}\big(
         \frac{X_{ij}+X_{kj}}{2}\big)-X_{ij}log_{10}X_{kj}-X_{kj}log_{10}X_{ij}}
         {X_{ij}+X_{kj}}\Bigg)
+
+    In the above formula, whenever :math:`X_{ij} = 0` or :math:`X_{kj} = 0`,
+    the value 0.1 is substituted.
+
+    Since this measure ranges from 0 to :math:`\infty`, a similarity measure,
+    CYs, ranging from 0 to 1 was also developed.
+
+    .. math::
+
+        CYs = 1 - \frac{Observed CYd}{Maximum CYd}
+
+    where
+
+    .. math::
+
+        Observed~CYd = \sum\Bigg(\frac{(X_{ij} + X_{kj})log_{10}\big(
+        \frac{X_{ij}+X_{kj}}{2}\big)-X_{ij}log_{10}X_{kj}-X_{kj}log_{10}X_{ij}}
+        {X_{ij}+X_{kj}}\Bigg)
+
+    and with :math:`a` (the number of species present in both samples),
+    :math:`b` (the number of species present in sample :math:`i` only), and
+    :math:`c` (the number of species present in sample :math:`j` only),
+
+    .. math::
+
+        Maximum~CYd = D_1 + D_2 + D_3
+
+    with
+
+    .. math::
+
+        D_1 = \sum_{j=1}^b \Bigg(\frac{(X_{ij} + 0.1 log_{10} \big(
+        \frac{X_{ij}+0.1}{2}\big)-X_{ij}log{10}(0.1)-0.1log_{10}(X_{ij})}
+        {X_{ij}+0.1}\Bigg)
+
+        D_2 = \sum_{j=1}^c \Bigg(\frac{(X_{kj} + 0.1 log_{10} \big(
+        \frac{X_{kj}+0.1}{2}\big)-X_{kj}log{10}(0.1)-0.1log_{10}(X_{kj})}
+        {X_{kj}+0.1}\Bigg)
+
+        D_1 = \sum_{j=1}^a \frac{a}{2} \Bigg(\frac{(D_i + 1) log_{10}
+        \big(\frac{D_i+1}{2}\big)-log_{10}(D_i)}{D_i+1} + \frac{(D_k + 1 log_{10}
+        \big(\frac{D_k+1}{2}\big)-log_{10}(D_k)}{D_k+1}\Bigg)
+
+    with
+
+    .. math::
+
+        D_i = \frac{\sum X_{ij} - \frac{a}{2}}{\frac{a}{2}}
+
+        D_k = \frac{\sum X_{kj} - \frac{a}{2}}{\frac{a}{2}}
+
+    for
+
+    .. math::
+
+        X_{ij} \geq 1
+
+        X_{kj} \geq 1
 
     .. versionadded:: 0.4.1
     """
