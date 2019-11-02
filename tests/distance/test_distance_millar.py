a3b70b49d6eb7eb7f627bb09d0656945e399da70
Chris Little <chrisclittle@gmail.com>
2019-11-02 13:56:57 -0700

    added Clark/Millar test stubs

---
+++ tests/distance/test_distance_millar.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 by Christopher C. Little.
+# This file is part of Abydos.
+#
+# Abydos is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Abydos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Abydos. If not, see <http://www.gnu.org/licenses/>.
+
+"""abydos.tests.distance.test_distance_millar.
+
+This module contains unit tests for abydos.distance.Millar
+"""
+
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+import unittest
+
+from abydos.distance import Millar
+
+
+class MillarTestCases(unittest.TestCase):
+    """Test Millar functions.
+
+    abydos.distance.Millar
+    """
+
+    cmp = Millar()
+
+    def test_inclusion_dist(self):
+        """Test abydos.distance.Millar.dist."""
+        self.assertEqual(self.cmp.dist('', ''), 1.0)
+        self.assertEqual(self.cmp.dist('a', ''), 0.0)
+        self.assertEqual(self.cmp.dist('', 'a'), 0.0)
+        self.assertEqual(self.cmp.dist('a', 'a'), 1.0)
+        self.assertEqual(self.cmp.dist('abc', ''), 0.0)
+        self.assertEqual(self.cmp.dist('', 'abc'), 0.0)
+        self.assertEqual(self.cmp.dist('abc', 'abc'), 1.0)
+        self.assertEqual(self.cmp.dist('abcd', 'efgh'), 0.0)
+
+        self.assertAlmostEqual(self.cmp.dist('Nigel', 'Niall'), 0.0114358323)
+        self.assertAlmostEqual(self.cmp.dist('Niall', 'Nigel'), 0.0114358323)
+        self.assertAlmostEqual(self.cmp.dist('Colin', 'Coiln'), 0.0114358323)
+        self.assertAlmostEqual(self.cmp.dist('Coiln', 'Colin'), 0.0114358323)
+        self.assertAlmostEqual(
+            self.cmp.dist('ATCAACGAGT', 'AACGATTAG'), 0.0139593909
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
