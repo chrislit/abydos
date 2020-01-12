13b6c73a15791561af1eeb92ac517a93d5cb7c1f
Chris Little <chrisclittle@gmail.com>
2020-01-12 13:30:21 -0800

    though not previously deprecated, removed sim & dist functions
    These don't make any sense any more, so removing them.

--- abydos/distance/__init__.py
+++ abydos/distance/__init__.py
@@ -643,8 +643,6 @@ from ._yule_y import YuleY
 __all__ = [
     '_Distance',
     '_TokenDistance',
-    'sim',
-    'dist',
     'Levenshtein',
     'DamerauLevenshtein',
     'ShapiraStorerI',
@@ -918,100 +916,6 @@ __all__ = [
     'LIG3',
     'SSK',
 ]
-
-
-def sim(src, tar, method=None):
-    """Return a similarity of two strings.
-
-    This is a generalized function for calling other similarity functions.
-
-    Parameters
-    ----------
-    src : str
-        Source string for comparison
-    tar : str
-        Target string for comparison
-    method : function
-        Specifies the similarity metric (:py:meth:`Levenshtein.sim` by default)
-
-    Returns
-    -------
-    float
-        Similarity according to the specified function
-
-    Raises
-    ------
-    AttributeError
-        Unknown distance function
-
-    Examples
-    --------
-    >>> round(sim('cat', 'hat'), 12)
-    0.666666666667
-    >>> round(sim('Niall', 'Neil'), 12)
-    0.4
-    >>> sim('aluminum', 'Catalan')
-    0.125
-    >>> sim('ATCG', 'TAGC')
-    0.25
-
-    .. versionadded:: 0.1.0
-
-    """
-    if method is None:
-        method = Levenshtein().sim
-    if callable(method):
-        return method(src, tar)
-    else:
-        raise AttributeError('Unknown similarity function: ' + str(method))
-
-
-def dist(src, tar, method=None):
-    """Return a distance between two strings.
-
-    This is a generalized function for calling other distance functions.
-
-    Parameters
-    ----------
-    src : str
-        Source string for comparison
-    tar : str
-        Target string for comparison
-    method : function
-        Specifies the similarity metric (:py:meth:`Levenshtein.sim` by default)
-        -- Note that this takes a similarity metric function, not a distance
-        metric function.
-
-    Returns
-    -------
-    float
-        Distance according to the specified function
-
-    Raises
-    ------
-    AttributeError
-        Unknown distance function
-
-    Examples
-    --------
-    >>> round(dist('cat', 'hat'), 12)
-    0.333333333333
-    >>> round(dist('Niall', 'Neil'), 12)
-    0.6
-    >>> dist('aluminum', 'Catalan')
-    0.875
-    >>> dist('ATCG', 'TAGC')
-    0.75
-
-    .. versionadded:: 0.1.0
-
-    """
-    if method is None:
-        method = Levenshtein().sim
-    if callable(method):
-        return 1 - method(src, tar)
-    else:
-        raise AttributeError('Unknown distance function: ' + str(method))
 
 
 if __name__ == '__main__':
