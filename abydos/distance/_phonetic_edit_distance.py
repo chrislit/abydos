9c18833f13491584c64ec5af63079f2b3e2f06ff
Oleg Serikov <srkvoa@gmail.com>
2022-09-14 14:23:39 +0300

    Update _phonetic_edit_distance.py

--- abydos/distance/_phonetic_edit_distance.py
+++ abydos/distance/_phonetic_edit_distance.py
@@ -141,11 +141,11 @@ class PhoneticEditDistance(Levenshtein):
         """
         ins_cost, del_cost, sub_cost, trans_cost = self._cost
 
-        src_len = len(src)
-        tar_len = len(tar)
-
         src_list = ipa_to_features(src)
         tar_list = ipa_to_features(tar)
+
+        src_len = len(src_list)
+        tar_len = len(tar_list)
 
         d_mat = np.zeros((src_len + 1, tar_len + 1), dtype=np.float_)
         if backtrace:
